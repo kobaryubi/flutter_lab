6eb1a5ed2f8a08e6440dc0ed4bd8d0d6b152c1b5
Masahiko Kobayashi <kobaryubi6@gmail.com>
2025-07-28 12:24:33 +0900

    feat: lab

--- lib/main.dart
+++ lib/main.dart
@@ -42,9 +42,14 @@ class FlutterLabModel extends ChangeNotifier {
   }
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
