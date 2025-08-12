c530e09007cfd379ecdc88e56dff403d756200ea
Masahiko Kobayashi <kobaryubi6@gmail.com>
2025-08-12 20:55:01 +0900

    feat: lab

--- lib/compass_app.dart
+++ lib/compass_app.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_lab/ui/home/widgets/home_screen.dart';
+import 'package:go_router/go_router.dart';
+
+final _router = GoRouter(
+  routes: [GoRoute(path: '/', builder: (context, state) => const HomeScreen())],
+);
 
 class CompassApp extends StatelessWidget {
   const CompassApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Text('Compass App');
+    return MaterialApp.router(routerConfig: _router);
   }
 }
