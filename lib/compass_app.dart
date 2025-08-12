807d6f7db52db2cb367c7944e359386e5dd4eb57
Masahiko Kobayashi <kobaryubi6@gmail.com>
2025-08-12 19:09:21 +0900

    feat: lab

---
+++ lib/compass_app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CompassApp extends StatelessWidget {
+  const CompassApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text('Compass App');
+  }
+}
