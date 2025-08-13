7b49526961ea001707edff91795d1e60aad73a96
Masahiko Kobayashi <kobaryubi6@gmail.com>
2025-08-13 15:23:04 +0900

    feat: lab

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_lab/compass_app.dart';
 import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const FlutterLabApp());
+  runApp(MultiProvider(providers: [], child: const CompassApp()));
 }
 
 class FlutterLabApp extends StatelessWidget {
