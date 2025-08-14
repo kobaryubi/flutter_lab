e95755e141b15cf0a5f536882bfce70ce737306e
Masahiko Kobayashi <kobaryubi6@gmail.com>
2025-08-14 17:18:53 +0900

    feat: lab

---
+++ lib/domain/models/itinerary_config/itinerary_config.dart
@@ -0,0 +1,19 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'itinerary_config.freezed.dart';
+part 'itinerary_config.g.dart';
+
+@freezed
+abstract class ItineraryConfig with _$ItineraryConfig {
+  const factory ItineraryConfig({
+    String? continent,
+    DateTime? startDate,
+    DateTime? endDate,
+    int? guests,
+    String? destination,
+    @Default([]) List<String> activities,
+  }) = _ItineraryConfig;
+
+  factory ItineraryConfig.fromJson(Map<String, Object?> json) =>
+      _$ItineraryConfigFromJson(json);
+}
