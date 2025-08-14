8acce64c0b4e372eba14ac7e720357c9522b4ab7
Masahiko Kobayashi <kobaryubi6@gmail.com>
2025-08-14 12:15:10 +0900

    feat: lab

---
+++ lib/utils/result.dart
@@ -0,0 +1,33 @@
+// /// switch (result) {
+// ///   case Ok(): {
+// ///     print(result.value);
+// ///   }
+// ///   case Error(): {
+// ///     print(result.error);
+// ///   }
+// /// }
+// /// ```
+sealed class Result<T> {
+  const Result();
+
+  const factory Result.ok(T value) = Ok._;
+  const factory Result.error(Exception error) = Error._;
+}
+
+final class Ok<T> extends Result<T> {
+  const Ok._(this.value);
+
+  final T value;
+
+  @override
+  String toString() => 'Result<$T>.ok($value)';
+}
+
+final class Error<T> extends Result<T> {
+  const Error._(this.error);
+
+  final Exception error;
+
+  @override
+  String toString() => 'Result<$T>.error($error)';
+}
