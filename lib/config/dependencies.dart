19bf7a05845ac042cc2fcdd448769774c1458e07
Masahiko Kobayashi <kobaryubi6@gmail.com>
2025-08-14 17:26:20 +0900

    feat: lab

--- lib/config/dependencies.dart
+++ lib/config/dependencies.dart
@@ -1,3 +1,5 @@
+import 'package:flutter_lab/data/repositories/itinerary_config/itinerary_config_repository.dart';
+import 'package:flutter_lab/data/repositories/itinerary_config/itinerary_config_repository_memory.dart';
 import 'package:flutter_lab/data/repositories/user/user_repository.dart';
 import 'package:flutter_lab/data/repositories/user/user_repository_local.dart';
 import 'package:flutter_lab/data/services/local/local_data_service.dart';
@@ -6,6 +8,9 @@ import 'package:provider/single_child_widget.dart';
 
 final List<SingleChildWidget> providersLocal = [
   Provider(create: (_) => LocalDataService()),
+  Provider<ItineraryConfigRepository>(
+    create: (_) => ItineraryConfigRepositoryMemory(),
+  ),
   Provider<UserRepository>(
     create: (context) => UserRepositoryLocal(localDataService: context.read()),
   ),
