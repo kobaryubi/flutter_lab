d0a150275e4f1deb311a8e512f0ae29de465da45
Masahiko Kobayashi <kobaryubi6@gmail.com>
2025-08-14 20:12:47 +0900

    feat: lab

---
+++ lib/data/services/local/local_data_service.dart
@@ -0,0 +1,62 @@
+import 'dart:convert';
+
+import 'package:flutter/services.dart';
+import 'package:flutter_lab/config/assets.dart';
+import 'package:flutter_lab/domain/models/activity/activity.dart';
+import 'package:flutter_lab/domain/models/continent/continent.dart';
+import 'package:flutter_lab/domain/models/destination/destination.dart';
+import 'package:flutter_lab/domain/models/user/user.dart';
+
+class LocalDataService {
+  User getUser() {
+    return const User(name: 'Sofie', picture: 'assets/user.jpg');
+  }
+
+  List<Continent> getContinents() {
+    return [
+      const Continent(
+        name: 'Europe',
+        imageUrl: 'https://rstr.in/google/tripedia/TmR12QdlVTT',
+      ),
+      const Continent(
+        name: 'Asia',
+        imageUrl: 'https://rstr.in/google/tripedia/VJ8BXlQg8O1',
+      ),
+      const Continent(
+        name: 'South America',
+        imageUrl: 'https://rstr.in/google/tripedia/flm_-o1aI8e',
+      ),
+      const Continent(
+        name: 'Africa',
+        imageUrl: 'https://rstr.in/google/tripedia/-nzi8yFOBpF',
+      ),
+      const Continent(
+        name: 'North America',
+        imageUrl: 'https://rstr.in/google/tripedia/jlbgFDrSUVE',
+      ),
+      const Continent(
+        name: 'Oceania',
+        imageUrl: 'https://rstr.in/google/tripedia/vxyrDE-fZVL',
+      ),
+      const Continent(
+        name: 'Australia',
+        imageUrl: 'https://rstr.in/google/tripedia/z6vy6HeRyvZ',
+      ),
+    ];
+  }
+
+  Future<List<Activity>> getActivities() async {
+    final json = await _loadStringAsset(Assets.activities);
+    return json.map<Activity>(Activity.fromJson).toList();
+  }
+
+  Future<List<Destination>> getDestinations() async {
+    final json = await _loadStringAsset(Assets.destinations);
+    return json.map<Destination>(Destination.fromJson).toList();
+  }
+
+  Future<List<Map<String, dynamic>>> _loadStringAsset(String asset) async {
+    final localData = await rootBundle.loadString(asset);
+    return (jsonDecode(localData) as List).cast<Map<String, dynamic>>();
+  }
+}
