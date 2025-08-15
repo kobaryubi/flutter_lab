d75d47878b74b9994ac2e43f8b466cf89f2747a0
Masahiko Kobayashi <kobaryubi6@gmail.com>
2025-08-15 14:53:06 +0900

    feat: lab

---
+++ lib/ui/core/themes/colors.dart
@@ -0,0 +1,20 @@
+import 'dart:ui';
+
+abstract final class CompassAppColors {
+  static const primary = Color(0xff3d6838);
+  static const onPrimary = Color(0xffffffff);
+  static const primaryContainer = Color(0xffbdf0b3);
+  static const onPrimaryContainer = Color(0xff255023);
+  static const secondary = Color(0xff53634e);
+  static const onSecondary = Color(0xffffffff);
+  static const secondaryContainer = Color(0xffd6e8ce);
+  static const onSecondaryContainer = Color(0xff3c4b38);
+  static const tertiary = Color(0xff38656a);
+  static const onTertiary = Color(0xffffffff);
+  static const tertiaryContainer = Color(0xffbcebf0);
+  static const onTertiaryContainer = Color(0xff1e4d52);
+  static const error = Color(0xffba1a1a);
+  static const onError = Color(0xffffffff);
+  static const errorContainer = Color(0xffffdad6);
+  static const onErrorContainer = Color(0xff93000a);
+}
