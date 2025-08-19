6710a252ee7815f7d50dfca7a58290468b83fa76
Masahiko Kobayashi <kobaryubi6@gmail.com>
2025-08-20 06:03:31 +0900

    feat: lab

---
+++ lib/ui/optimistic_state/widgets/optimistic_state_view_model.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/widgets.dart';
+import 'package:flutter_lab/data/repositories/subscription/subscription_repository.dart';
+import 'package:flutter_lab/utils/command.dart';
+import 'package:flutter_lab/utils/result.dart';
+import 'package:logging/logging.dart';
+
+class OptimisticStateViewModel extends ChangeNotifier {
+  OptimisticStateViewModel({
+    required SubscriptionRepository subscriptionRepository,
+  }) : _subscriptionRepository = subscriptionRepository {
+    subscribe = Command0(_subscribe);
+  }
+
+  final SubscriptionRepository _subscriptionRepository;
+
+  final _log = Logger('OptimisticStateViewModel');
+  bool _subscribed = false;
+  bool get subscribed => _subscribed;
+
+  late final Command0 subscribe;
+
+  Future<Result<void>> _subscribe() async {
+    if (_subscribed) {
+      _log.warning('Already subscribed');
+      return Result.error(Exception('Already subscribed'));
+    }
+
+    _subscribed = true;
+    notifyListeners();
+
+    try {
+      final resultSubscribe = await _subscriptionRepository.subscribe();
+      switch (resultSubscribe) {
+        case Ok<void>():
+          _log.fine('Subscribed successfully');
+        case Error<void>():
+          _subscribed = false;
+          _log.warning('Failed to subscribe', resultSubscribe.error);
+      }
+
+      return resultSubscribe;
+    } finally {
+      notifyListeners();
+    }
+  }
+}
