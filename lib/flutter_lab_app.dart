b644745a42335ca927419f21185c0c3068593639
Masahiko Kobayashi <kobaryubi6@gmail.com>
2025-08-19 17:34:46 +0900

    feat: lab

--- lib/flutter_lab_app.dart
+++ lib/flutter_lab_app.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_lab/l10n/app_localizations.dart';
 import 'package:flutter_lab/routing/router.dart';
+import 'package:flutter_lab/ui/core/themes/colors.dart';
+import 'package:flutter_lab/ui/core/themes/theme.dart';
 import 'package:provider/provider.dart';
 
 class FlutterLabApp extends StatelessWidget {
@@ -10,10 +12,13 @@ class FlutterLabApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return WidgetsApp.router(
       routerConfig: router(context.read()),
-      color: const Color(0x00ffffff),
+      color: const Color(0xffffffff),
       title: 'Flutter Lab',
       localizationsDelegates: AppLocalizations.localizationsDelegates,
       supportedLocales: AppLocalizations.supportedLocales,
+      textStyle: TextStyles.bodyMedium.merge(
+        TextStyle(color: AppColors.black1),
+      ),
     );
   }
 }
