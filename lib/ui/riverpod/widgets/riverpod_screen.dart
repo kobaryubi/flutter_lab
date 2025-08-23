4b1b4bd92cfb374c020211fea6925a2d53a34db2
Masahiko Kobayashi <kobaryubi6@gmail.com>
2025-08-24 05:28:08 +0900

    feat: lab

---
+++ lib/ui/riverpod/widgets/riverpod_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/widgets.dart';
+
+class RiverpodScreen extends StatelessWidget {
+  const RiverpodScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Text('Riverpod Screen'));
+  }
+}
