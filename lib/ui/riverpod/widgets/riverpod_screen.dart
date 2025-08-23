52dbb55db4d2862436384aaf82cec115996f0d3d
Masahiko Kobayashi <kobaryubi6@gmail.com>
2025-08-24 05:31:30 +0900

    feat: lab

--- lib/ui/riverpod/widgets/riverpod_screen.dart
+++ lib/ui/riverpod/widgets/riverpod_screen.dart
@@ -1,10 +1,24 @@
 import 'package:flutter/widgets.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
 
-class RiverpodScreen extends StatelessWidget {
+part 'riverpod_screen.g.dart';
+
+@riverpod
+String helloWorld(Ref ref) {
+  return 'Hello, World!';
+}
+
+class RiverpodScreen extends HookConsumerWidget {
   const RiverpodScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return Center(child: Text('Riverpod Screen'));
+  Widget build(BuildContext context, WidgetRef ref) {
+    final counter = useState(0);
+
+    final String value = ref.watch(helloWorldProvider);
+
+    return Center(child: Text('$value ${counter.value}'));
   }
 }
