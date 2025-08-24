e2e68b31bf26915db512ac380e6cf37418a5d0fe
Masahiko Kobayashi <kobaryubi6@gmail.com>
2025-08-24 13:35:15 +0900

    feat: lab

---
+++ lib/ui/core/ui/selection_area.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/widgets.dart';
+
+class SelectionArea extends StatefulWidget {
+  const SelectionArea({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _SelectionAreaState();
+}
+
+class _SelectionAreaState extends State<SelectionArea> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
