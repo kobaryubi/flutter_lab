c7384178e5efdce1bbb2ef94f585ce307c1f282e
Masahiko Kobayashi <kobaryubi6@gmail.com>
2025-08-24 13:49:27 +0900

    feat: lab

---
+++ lib/ui/core/ui/elevated_button.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/widgets.dart';
+
+class ElevatedButton extends StatefulWidget {
+  const ElevatedButton({super.key});
+
+  @override
+  State<ElevatedButton> createState() => _ElevatedButtonState();
+}
+
+class _ElevatedButtonState extends State<ElevatedButton> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
