a6a536af135064dca446d3745b9906353f21c1e9
Masahiko Kobayashi <kobaryubi6@gmail.com>
2025-08-25 15:26:20 +0900

    feat: lab

---
+++ lib/domain/models/todo/todo.g.dart
@@ -0,0 +1,19 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'todo.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_Todo _$TodoFromJson(Map<String, dynamic> json) => _Todo(
+  description: json['description'] as String,
+  id: (json['id'] as num).toInt(),
+  completed: json['completed'] as bool? ?? false,
+);
+
+Map<String, dynamic> _$TodoToJson(_Todo instance) => <String, dynamic>{
+  'description': instance.description,
+  'id': instance.id,
+  'completed': instance.completed,
+};
