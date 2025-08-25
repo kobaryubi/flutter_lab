2033a0684ec37470dcdcfeca5b81fc3ba41efd00
Masahiko Kobayashi <kobaryubi6@gmail.com>
2025-08-26 05:45:34 +0900

    feat: lab

---
+++ lib/ui/core/ui/text_button.dart
@@ -0,0 +1,5 @@
+import 'package:flutter_lab/ui/core/ui/button_style_button.dart';
+
+class TextButton extends ButtonStyleButton {
+  const TextButton({super.key, required super.onPressed, required super.child});
+}
