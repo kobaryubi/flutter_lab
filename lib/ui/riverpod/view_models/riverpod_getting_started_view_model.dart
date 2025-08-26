d572bbec59c3b590c5128470c4d7bea8d740f7f0
Masahiko Kobayashi <kobaryubi6@gmail.com>
2025-08-27 06:34:12 +0900

    feat: lab

--- lib/ui/riverpod/view_models/riverpod_getting_started_view_model.dart
+++ lib/ui/riverpod/view_models/riverpod_getting_started_view_model.dart
@@ -5,18 +5,41 @@ import 'package:riverpod_annotation/riverpod_annotation.dart';
 part 'riverpod_getting_started_view_model.g.dart';
 
 @riverpod
+/// Notifier for the todo list filter.
+class TodoListFilterNotifier extends _$TodoListFilterNotifier {
+  @override
+  TodoListFilter build() => TodoListFilter.all;
+
+  @override
+  set state(TodoListFilter newState) => super.state = newState;
+}
+
+@riverpod
 /// ViewModel for the Riverpod Getting Started screen.
 final class RiverpodGettingStartedViewModel
     extends _$RiverpodGettingStartedViewModel {
   @override
   RiverpodGettingStartedUiState build() {
-    final uncompletedTodosCount = ref
-        .watch(todoListNotifierProvider)
-        .where((todo) => !todo.completed)
-        .length;
+    final todos = ref.watch(todoListNotifierProvider);
+    final filter = ref.watch(todoListFilterNotifierProvider);
+    final uncompletedTodosCount = todos.where((todo) => !todo.completed).length;
+
+    final filteredTodos = switch (filter) {
+      TodoListFilter.completed =>
+        todos.where((todo) => todo.completed).toList(),
+      TodoListFilter.active => todos.where((todo) => !todo.completed).toList(),
+      TodoListFilter.all => todos,
+    };
 
     return RiverpodGettingStartedUiState(
       uncompletedTodosCount: uncompletedTodosCount,
+      filter: filter,
+      filteredTodos: filteredTodos,
     );
+  }
+
+  /// Updates the UI state based on the current filter.
+  void updateFilter(TodoListFilter filter) {
+    state = state.copyWith(filter: filter);
   }
 }
