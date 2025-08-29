96a060ae05123e700b235d62371ec7985e501ada
Masahiko Kobayashi <kobaryubi6@gmail.com>
2025-08-29 20:30:05 +0900

    feat: lab

---
+++ lib/ui/core/ui/scaffold.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/widgets.dart';
+
+class Scaffold extends StatefulWidget {
+  const Scaffold({super.key});
+
+  @override
+  State<Scaffold> createState() => _ScaffoldState();
+}
+
+class _ScaffoldState extends State<Scaffold> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
