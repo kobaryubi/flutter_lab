788817ff08892af3bbbe5e5de7fb086217f243c2
Masahiko Kobayashi <kobaryubi6@gmail.com>
2025-08-29 20:03:51 +0900

    feat: lab

--- lib/ui/compass/widgets/compass_screen.dart
+++ lib/ui/compass/widgets/compass_screen.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/widgets.dart';
+import 'package:flutter_lab/l10n/app_localizations.dart';
 import 'package:flutter_lab/ui/home/home_view_model.dart';
 
 class CompassScreen extends StatefulWidget {
-  const CompassScreen({super.key, required this.viewModel});
+  const CompassScreen({required HomeViewModel viewModel, super.key})
+    : _viewModel = viewModel;
 
-  final HomeViewModel viewModel;
+  final HomeViewModel _viewModel;
 
   @override
   State<CompassScreen> createState() => _CompassScreenState();
@@ -12,7 +14,40 @@ class CompassScreen extends StatefulWidget {
 
 class _CompassScreenState extends State<CompassScreen> {
   @override
+  void initState() {
+    super.initState();
+    widget._viewModel.deleteBooking.addListener(_onResult);
+  }
+
+  @override
+  void didUpdateWidget(covariant CompassScreen oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    oldWidget._viewModel.deleteBooking.removeListener(_onResult);
+    widget._viewModel.deleteBooking.addListener(_onResult);
+  }
+
+  @override
+  void dispose() {
+    widget._viewModel.deleteBooking.removeListener(_onResult);
+    super.dispose();
+  }
+
+  void _onResult() {
+    if (widget._viewModel.deleteBooking.completed) {
+      widget._viewModel.deleteBooking.clearResult();
+    }
+
+    if (widget._viewModel.deleteBooking.error) {
+      widget._viewModel.deleteBooking.clearResult();
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final appLocalizations = AppLocalizations.of(context);
+    if (appLocalizations == null) {
+      return const Center(child: Text('Localization not found'));
+    }
     return Text('compass screen');
   }
 }
