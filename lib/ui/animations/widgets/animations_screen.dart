c11d8fb01d3c9b8435f7222882fa36f6c63abe87
Masahiko Kobayashi <kobaryubi6@gmail.com>
2025-08-30 18:11:04 +0900

    feat: lab

---
+++ lib/ui/animations/widgets/animations_screen.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/widgets.dart';
+import 'package:flutter_lab/ui/core/ui/scaffold.dart';
+
+class AnimatedLogo extends AnimatedWidget {
+  const AnimatedLogo({super.key, required Animation<double> animation})
+    : super(listenable: animation);
+
+  @override
+  Widget build(BuildContext context) {
+    final animation = listenable as Animation<double>;
+
+    return Center(
+      child: Container(
+        margin: const EdgeInsets.symmetric(vertical: 10),
+        height: animation.value,
+        width: animation.value,
+        child: const FlutterLogo(),
+      ),
+    );
+  }
+}
+
+class AnimationsScreen extends StatefulWidget {
+  const AnimationsScreen({super.key});
+
+  @override
+  State<AnimationsScreen> createState() => _AnimationsScreenState();
+}
+
+class _AnimationsScreenState extends State<AnimationsScreen>
+    with SingleTickerProviderStateMixin {
+  late Animation<double> animation;
+  late AnimationController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = AnimationController(
+      duration: const Duration(seconds: 2),
+      vsync: this,
+    );
+    animation = Tween<double>(begin: 0, end: 300).animate(controller);
+    controller.forward();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: AnimatedLogo(
+        animation: animation,
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+}
