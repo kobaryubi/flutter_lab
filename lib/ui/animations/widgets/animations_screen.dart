507c2d36656216034d4ad417e3cb6af26ca40561
Masahiko Kobayashi <kobaryubi6@gmail.com>
2025-08-31 07:06:52 +0900

    feat: lab

--- lib/ui/animations/widgets/animations_screen.dart
+++ lib/ui/animations/widgets/animations_screen.dart
@@ -2,19 +2,65 @@ import 'package:flutter/widgets.dart';
 import 'package:flutter_lab/ui/core/ui/scaffold.dart';
 
 class AnimatedLogo extends AnimatedWidget {
-  const AnimatedLogo({super.key, required Animation<double> animation})
+  const AnimatedLogo({required Animation<double> animation, super.key})
     : super(listenable: animation);
+
+  static final _opacityTween = Tween<double>(begin: 0.1, end: 1);
+  static final _sizeTween = Tween<double>(begin: 0, end: 300);
 
   @override
   Widget build(BuildContext context) {
     final animation = listenable as Animation<double>;
 
     return Center(
-      child: Container(
-        margin: const EdgeInsets.symmetric(vertical: 10),
-        height: animation.value,
-        width: animation.value,
-        child: const FlutterLogo(),
+      child: Opacity(
+        opacity: _opacityTween.evaluate(animation),
+        child: Container(
+          margin: const EdgeInsets.symmetric(vertical: 10),
+          height: _sizeTween.evaluate(animation),
+          width: _sizeTween.evaluate(animation),
+          child: const FlutterLogo(),
+        ),
+      ),
+    );
+  }
+}
+
+class LogoWidget extends StatelessWidget {
+  const LogoWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 10),
+      child: const FlutterLogo(),
+    );
+  }
+}
+
+class GrowTransition extends StatelessWidget {
+  const GrowTransition({
+    required this.child,
+    required this.animation,
+    super.key,
+  });
+
+  final Widget child;
+  final Animation<double> animation;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: AnimatedBuilder(
+        animation: animation,
+        builder: (context, child) {
+          return SizedBox(
+            height: animation.value,
+            width: animation.value,
+            child: child,
+          );
+        },
+        child: child,
       ),
     );
   }
@@ -39,17 +85,20 @@ class _AnimationsScreenState extends State<AnimationsScreen>
       duration: const Duration(seconds: 2),
       vsync: this,
     );
-    animation = Tween<double>(begin: 0, end: 300).animate(controller);
+    animation = CurvedAnimation(parent: controller, curve: Curves.easeIn)
+      ..addStatusListener((status) {
+        if (status == AnimationStatus.completed) {
+          controller.reverse();
+        } else if (status == AnimationStatus.dismissed) {
+          controller.forward();
+        }
+      });
     controller.forward();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: AnimatedLogo(
-        animation: animation,
-      ),
-    );
+    return Scaffold(body: AnimatedLogo(animation: animation));
   }
 
   @override
