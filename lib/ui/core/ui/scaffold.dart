df8d7e8f2e2882e4e24e1f6f45e501be5c34ba61
Masahiko Kobayashi <kobaryubi6@gmail.com>
2025-08-30 13:55:47 +0900

    feat: lab

--- lib/ui/core/ui/scaffold.dart
+++ lib/ui/core/ui/scaffold.dart
@@ -1,22 +1,16 @@
 import 'package:flutter/widgets.dart';
 import 'package:flutter_lab/ui/core/themes/colors.dart';
 
-class Scaffold extends StatefulWidget {
-  const Scaffold({this.drawer, super.key});
+class Scaffold extends StatelessWidget {
+  const Scaffold({required this.child, super.key});
 
-  final Widget? drawer;
+  final Widget child;
 
-  @override
-  State<Scaffold> createState() => _ScaffoldState();
-}
-
-class _ScaffoldState extends State<Scaffold> {
   @override
   Widget build(BuildContext context) {
-    return const ScrollNotificationObserver(
-      child: ColoredBox(
-        color: AppColors.scaffoldBackgroundColor,
-      ),
+    return ColoredBox(
+      color: AppColors.scaffoldBackgroundColor,
+      child: SafeArea(child: child),
     );
   }
 }
