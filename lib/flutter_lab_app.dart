078b0aeb2fc36b4b0317984fd37c881a848de7aa
Masahiko Kobayashi <kobaryubi6@gmail.com>
2025-08-31 20:30:15 +0900

    feat: lab

--- lib/flutter_lab_app.dart
+++ lib/flutter_lab_app.dart
@@ -3,13 +3,14 @@ import 'package:flutter_lab/l10n/app_localizations.dart';
 import 'package:flutter_lab/routing/router.dart';
 import 'package:flutter_lab/ui/core/themes/colors.dart';
 import 'package:flutter_lab/ui/core/themes/theme.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:provider/provider.dart';
 
-class FlutterLabApp extends StatelessWidget {
+class FlutterLabApp extends ConsumerWidget {
   const FlutterLabApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return WidgetsApp.router(
       routerConfig: router(context.read()),
       color: const Color(0xffffffff),
