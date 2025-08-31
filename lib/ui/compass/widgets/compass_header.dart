a8df1b45b6a5cc5abc6e1959c1c3ebf529148a86
Masahiko Kobayashi <kobaryubi6@gmail.com>
2025-08-31 15:16:20 +0900

    feat: lab

---
+++ lib/ui/compass/widgets/compass_header.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/widgets.dart';
+import 'package:flutter_lab/ui/compass/view_models/compass_view_model.dart';
+
+class CompassHeader extends StatelessWidget {
+  const CompassHeader({super.key, required this.viewModel});
+
+  final CompassViewModel viewModel;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
