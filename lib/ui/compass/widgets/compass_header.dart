1714527f58c4f30421847ce0d674b1987a5a8ffc
Masahiko Kobayashi <kobaryubi6@gmail.com>
2025-08-31 20:15:40 +0900

    feat: lab

--- lib/ui/compass/widgets/compass_header.dart
+++ lib/ui/compass/widgets/compass_header.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/widgets.dart';
+import 'package:flutter_lab/l10n/app_localizations.dart';
 import 'package:flutter_lab/ui/compass/view_models/compass_view_model.dart';
+import 'package:flutter_lab/ui/core/themes/dimens.dart';
 
 class CompassHeader extends StatelessWidget {
   const CompassHeader({super.key, required this.viewModel});
@@ -8,10 +10,30 @@ class CompassHeader extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final appLocalizations = AppLocalizations.of(context);
+    if (appLocalizations == null) {
+      return const Center(child: Text('Localization not found'));
+    }
+
     final user = viewModel.user;
     if (user == null) {
       return const SizedBox();
     }
-    return Container();
+
+    return Column(
+      children: [
+        Row(
+          children: [
+            ClipOval(
+              child: Image.asset(
+                user.picture,
+                width: Dimens.profilePictureSize,
+                height: Dimens.profilePictureSize,
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
   }
 }
