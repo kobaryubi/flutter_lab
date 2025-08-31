6e4ab8e8c693f9f3a6f20417763656737518e4d7
Masahiko Kobayashi <kobaryubi6@gmail.com>
2025-08-31 10:06:26 +0900

    feat: lab

--- lib/ui/core/ui/floating_action_button.dart
+++ lib/ui/core/ui/floating_action_button.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/widgets.dart';
+import 'package:flutter_lab/ui/core/themes/colors.dart';
+import 'package:flutter_lab/ui/core/themes/dimens.dart';
+import 'package:flutter_lab/ui/core/themes/theme.dart';
 
 class FloatingActionButton extends StatefulWidget {
   const FloatingActionButton({
@@ -19,11 +22,29 @@ class _FloatingActionButtonState extends State<FloatingActionButton> {
   Widget build(BuildContext context) {
     final button = GestureDetector(
       onTap: widget.onPressed,
-      child: IconTheme(
-        data: const IconThemeData(),
-        child: DefaultTextStyle(
-          style: const TextStyle(),
-          child: widget.child,
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(16),
+          color: AppColors.primaryContainer,
+          boxShadow: const [
+            BoxShadow(
+              color: AppColors.shadow,
+              blurRadius: 12,
+              offset: Offset(0, 6),
+            ),
+          ],
+        ),
+        child: IconTheme(
+          data: const IconThemeData(),
+          child: DefaultTextStyle(
+            style: TextStyles.bodySmall.copyWith(
+              color: AppColors.onPrimaryContainer,
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(Dimens.padding),
+              child: widget.child,
+            ),
+          ),
         ),
       ),
     );
