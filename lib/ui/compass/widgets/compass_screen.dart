dd868dcb1d73e47c68e8142f4cb931fc45af52fd
Masahiko Kobayashi <kobaryubi6@gmail.com>
2025-08-31 15:26:03 +0900

    feat: lab

--- lib/ui/compass/widgets/compass_screen.dart
+++ lib/ui/compass/widgets/compass_screen.dart
@@ -2,15 +2,17 @@ import 'package:flutter/widgets.dart';
 import 'package:flutter_lab/gen/assets.gen.dart';
 import 'package:flutter_lab/l10n/app_localizations.dart';
 import 'package:flutter_lab/routing/routes.dart';
+import 'package:flutter_lab/ui/compass/view_models/compass_view_model.dart';
+import 'package:flutter_lab/ui/compass/widgets/compass_header.dart';
+import 'package:flutter_lab/ui/core/themes/dimens.dart';
 import 'package:flutter_lab/ui/core/ui/floating_action_button.dart';
 import 'package:flutter_lab/ui/core/ui/scaffold.dart';
-import 'package:flutter_lab/ui/home/home_view_model.dart';
 import 'package:go_router/go_router.dart';
 
 class CompassScreen extends StatefulWidget {
   const CompassScreen({required this.viewModel, super.key});
 
-  final HomeViewModel viewModel;
+  final CompassViewModel viewModel;
 
   @override
   State<CompassScreen> createState() => _CompassScreenState();
@@ -58,7 +60,34 @@ class _CompassScreenState extends State<CompassScreen> {
     }
 
     return Scaffold(
-      body: const Text('compass screen'),
+      body: ListenableBuilder(
+        listenable: widget.viewModel.load,
+        builder: (context, child) {
+          if (child == null) {
+            return const Text('Loading...');
+          }
+
+          return child;
+        },
+        child: ListenableBuilder(
+          listenable: widget.viewModel,
+          builder: (context, _) {
+            return CustomScrollView(
+              slivers: [
+                SliverToBoxAdapter(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                      vertical: Dimens.paddingScreenVertical,
+                      horizontal: Dimens.paddingScreenHorizontal,
+                    ),
+                    child: CompassHeader(viewModel: widget.viewModel),
+                  ),
+                ),
+              ],
+            );
+          },
+        ),
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () => _onPressed(context),
         label: Text(appLocalizations.bookNewTrip),
