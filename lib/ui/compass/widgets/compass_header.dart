e58afb2fc24d78f249d74cda15a2478c74fdbaac
Masahiko Kobayashi <kobaryubi6@gmail.com>
2025-08-31 20:25:43 +0900

    feat: lab

--- lib/ui/compass/widgets/compass_header.dart
+++ lib/ui/compass/widgets/compass_header.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/widgets.dart';
 import 'package:flutter_lab/l10n/app_localizations.dart';
+import 'package:flutter_lab/ui/auth/logout/logout_view_model.dart';
+import 'package:flutter_lab/ui/auth/logout/widgets/logout_button.dart';
 import 'package:flutter_lab/ui/compass/view_models/compass_view_model.dart';
 import 'package:flutter_lab/ui/core/themes/dimens.dart';
+import 'package:provider/provider.dart';
 
 class CompassHeader extends StatelessWidget {
   const CompassHeader({super.key, required this.viewModel});
@@ -29,6 +32,12 @@ class CompassHeader extends StatelessWidget {
                 user.picture,
                 width: Dimens.profilePictureSize,
                 height: Dimens.profilePictureSize,
+              ),
+            ),
+            LogoutButton(
+              viewModel: LogoutViewModel(
+                authRepository: context.read(),
+                itineraryConfigRepository: context.read(),
               ),
             ),
           ],
