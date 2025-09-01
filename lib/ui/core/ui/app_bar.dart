9b4ce1aaedc3228205687127bb09541ecfcf696a
Masahiko Kobayashi <kobaryubi6@gmail.com>
2025-09-01 16:52:45 +0900

    feat: lab

--- lib/ui/core/ui/app_bar.dart
+++ lib/ui/core/ui/app_bar.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/widgets.dart';
+import 'package:flutter_lab/gen/assets.gen.dart';
 import 'package:flutter_lab/ui/core/themes/colors.dart';
 import 'package:flutter_lab/ui/core/themes/dimens.dart';
 import 'package:flutter_lab/ui/core/themes/theme.dart';
@@ -16,12 +17,48 @@ class AppBar extends StatelessWidget {
         bottom: false,
         child: SizedBox(
           height: Dimens.appBarHeight,
-          child: Center(
-            child: DefaultTextStyle(
-              style: TextStyles.headlineLarge.copyWith(
-                color: AppColors.onPrimary,
+          child: Stack(
+            children: [
+              Center(
+                child: DefaultTextStyle(
+                  style: TextStyles.headlineLarge.copyWith(
+                    color: AppColors.onPrimary,
+                  ),
+                  child: title,
+                ),
               ),
-              child: title,
+              const Align(
+                alignment: Alignment.centerLeft,
+                child: _BackButton(),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _BackButton extends StatelessWidget {
+  const _BackButton();
+
+  @override
+  Widget build(BuildContext context) {
+    return Semantics(
+      button: true,
+      label: 'back',
+      child: GestureDetector(
+        behavior: HitTestBehavior.opaque,
+        child: SizedBox.square(
+          dimension: Dimens.actionSize,
+          child: Center(
+            child: Assets.icons.arrowBack.svg(
+              width: Dimens.iconWidth,
+              height: Dimens.iconHeight,
+              colorFilter: const ColorFilter.mode(
+                AppColors.onPrimary,
+                BlendMode.srcIn,
+              ),
             ),
           ),
         ),
