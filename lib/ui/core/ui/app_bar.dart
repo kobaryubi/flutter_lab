909b88c02dbac54e6bcba0340c41725f07082a26
Masahiko Kobayashi <kobaryubi6@gmail.com>
2025-09-01 15:30:09 +0900

    feat: lab

---
+++ lib/ui/core/ui/app_bar.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/widgets.dart';
+
+class AppBar extends StatelessWidget {
+  const AppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
