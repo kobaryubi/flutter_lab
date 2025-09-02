b0bdd410ff680184dbada93ff1a1417f51a680da
Masahiko Kobayashi <kobaryubi6@gmail.com>
2025-09-02 20:22:07 +0900

    feat: lab

---
+++ lib/ui/core/ui/tag_chip.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/widgets.dart';
+
+class TagChip extends StatelessWidget {
+  const TagChip({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
