d94276f8c7424075c4ff79da7093110d0e7efb3d
Masahiko Kobayashi <kobaryubi6@gmail.com>
2025-09-02 12:28:14 +0900

    feat: lab

--- lib/config/dependencies.dart
+++ lib/config/dependencies.dart
@@ -26,6 +26,7 @@ import 'package:flutter_lab/data/repositories/user/user_repository_remote.dart';
 import 'package:flutter_lab/data/services/api/api_client.dart';
 import 'package:flutter_lab/data/services/dio/dio_client.dart';
 import 'package:flutter_lab/data/services/local/local_data_service.dart';
+import 'package:flutter_lab/domain/use_cases/booking/booking_share_use_case.dart';
 import 'package:provider/provider.dart';
 import 'package:provider/single_child_widget.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
@@ -46,10 +47,7 @@ List<SingleChildWidget> _sharedProviders = [
   //           bookingRepository: context.read(),
   //         ),
   //   ),
-  //   Provider(
-  //     lazy: true,
-  //     create: (context) => BookingShareUseCase.withSharePlus(),
-  //   ),
+  Provider(lazy: true, create: (_) => BookingShareUseCase.withSharePlus()),
 ];
 
 final List<SingleChildWidget> providersRemote = [
