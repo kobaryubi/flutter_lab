3ac5cf90e023f2e41e7c6eae9b100b6179a75024
Masahiko Kobayashi <kobaryubi6@gmail.com>
2025-09-03 06:00:29 +0900

    feat: lab

--- lib/ui/core/ui/tag_chip.dart
+++ lib/ui/core/ui/tag_chip.dart
@@ -1,10 +1,51 @@
+import 'dart:ui';
+
 import 'package:flutter/widgets.dart';
+import 'package:flutter_lab/gen/assets.gen.dart';
+import 'package:flutter_lab/ui/core/themes/colors.dart';
+import 'package:flutter_lab/ui/core/themes/theme.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class TagChip extends StatelessWidget {
-  const TagChip({super.key});
+  const TagChip({required this.tag, super.key});
+
+  final String tag;
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(16),
+      child: ColoredBox(
+        color: AppColors.primary,
+        child: SizedBox(
+          height: 32,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 6),
+            child: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Assets.icons.addLocationOutlined.svg(
+                  width: 16,
+                  height: 16,
+                  colorFilter: const ColorFilter.mode(
+                    AppColors.onPrimary,
+                    BlendMode.srcIn,
+                  ),
+                ),
+                const SizedBox(width: 4),
+                Text(
+                  tag,
+                  style: GoogleFonts.openSans(
+                    textStyle: TextStyles.bodySmall.copyWith(
+                      color: AppColors.onPrimary,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
