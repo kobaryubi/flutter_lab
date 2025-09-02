830de7e20be81ff079f5c16e67b02fcfaf2f76b3
Masahiko Kobayashi <kobaryubi6@gmail.com>
2025-09-02 11:17:18 +0900

    feat: lab

--- lib/ui/compass/widgets/compass_booking_screen.dart
+++ lib/ui/compass/widgets/compass_booking_screen.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/widgets.dart';
+import 'package:flutter_lab/gen/assets.gen.dart';
+import 'package:flutter_lab/l10n/app_localizations.dart';
 import 'package:flutter_lab/ui/compass/view_models/compass_booking_view_model.dart';
+import 'package:flutter_lab/ui/core/ui/floating_action_button.dart';
+import 'package:flutter_lab/ui/core/ui/scaffold.dart';
 
 class CompassBookingScreen extends StatefulWidget {
   const CompassBookingScreen({super.key, required this.viewModel});
@@ -13,6 +17,21 @@ class CompassBookingScreen extends StatefulWidget {
 class _CompassBookingScreenState extends State<CompassBookingScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final appLocalizations = AppLocalizations.of(context);
+    if (appLocalizations == null) {
+      return const SizedBox();
+    }
+
+    return Scaffold(
+      body: Container(),
+      floatingActionButton: ListenableBuilder(
+        listenable: widget.viewModel,
+        builder: (context, _) => FloatingActionButton(
+          onPressed: () {},
+          label: Text(appLocalizations.shareTrip),
+          icon: Assets.icons.share,
+        ),
+      ),
+    );
   }
 }
