0da06daca44b5fccd662aa55f10351c8143590a5
Masahiko Kobayashi <kobaryubi6@gmail.com>
2025-09-02 13:02:18 +0900

    feat: lab

--- lib/ui/compass/widgets/compass_booking_body.dart
+++ lib/ui/compass/widgets/compass_booking_body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/widgets.dart';
 import 'package:flutter_lab/ui/compass/view_models/compass_booking_view_model.dart';
+import 'package:flutter_lab/ui/compass/widgets/compass_booking_header.dart';
 
 class CompassBookingBody extends StatelessWidget {
   const CompassBookingBody({required this.viewModel, super.key});
@@ -14,7 +15,11 @@ class CompassBookingBody extends StatelessWidget {
         final booking = viewModel.booking;
         if (booking == null) return const SizedBox();
 
-        return const CustomScrollView(slivers: [SliverToBoxAdapter()]);
+        return const CustomScrollView(
+          slivers: [
+            SliverToBoxAdapter(child: CompassBookingHeader(booking: booking)),
+          ],
+        );
       },
     );
   }
---
+++ lib/ui/compass/widgets/compass_booking_header.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/widgets.dart';
+import 'package:flutter_lab/domain/models/booking/booking.dart';
+
+class CompassBookingHeader extends StatelessWidget {
+  const CompassBookingHeader({required this.booking, super.key});
+
+  final Booking booking;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
