5cc46e37da39c36c8542ef861caaad0de846e695
Masahiko Kobayashi <kobaryubi6@gmail.com>
2025-09-02 10:36:49 +0900

    feat: lab

---
+++ lib/ui/compass/widgets/compass_booking_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/widgets.dart';
+
+class CompassBookingScreen extends StatefulWidget {
+  const CompassBookingScreen({super.key});
+
+  @override
+  State<CompassBookingScreen> createState() => _CompassBookingScreenState();
+}
+
+class _CompassBookingScreenState extends State<CompassBookingScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
