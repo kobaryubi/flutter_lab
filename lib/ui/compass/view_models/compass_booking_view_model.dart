b8e6ee7203f9fef713587b93259666e240be8629
Masahiko Kobayashi <kobaryubi6@gmail.com>
2025-09-02 11:23:23 +0900

    feat: lab

--- lib/ui/compass/view_models/compass_booking_view_model.dart
+++ lib/ui/compass/view_models/compass_booking_view_model.dart
@@ -1,3 +1,10 @@
 import 'package:flutter/widgets.dart';
+import 'package:flutter_lab/domain/models/booking/booking.dart';
+import 'package:logging/logging.dart';
 
-class CompassBookingViewModel extends ChangeNotifier {}
+class CompassBookingViewModel extends ChangeNotifier {
+  Booking? _booking;
+  Booking? get booking => _booking;
+
+  final _log = Logger('CompassBookingViewModel');
+}
