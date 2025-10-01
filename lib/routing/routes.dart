03c2e46cd105796b8110a0072bab0f1451d56548
Masahiko Kobayashi <kobaryubi6@gmail.com>
2025-10-01 12:57:21 +0900

    feat: lab

--- lib/routing/routes.dart
+++ lib/routing/routes.dart
@@ -10,6 +10,8 @@ abstract final class Routes {
   static const booking = '/$bookingRelative';
   static const webview = '/$webviewRelative';
   static const webviewRelative = 'webview';
+  static const petstore = '/$petstoreRelative';
+  static const petstoreRelative = 'petstore';
 
   // compass
   static const compass = '/$compassRelative';
