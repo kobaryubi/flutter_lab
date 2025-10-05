548ecebc0c112b5829fd65c19055ad79d7676961
Masahiko Kobayashi <kobaryubi6@gmail.com>
2025-10-05 16:35:18 +0900

    feat: lab

--- lib/data/repositories/pet/pet_repository_remote.dart
+++ lib/data/repositories/pet/pet_repository_remote.dart
@@ -1,6 +1,7 @@
 import 'package:dio/dio.dart';
 import 'package:flutter_lab/data/mapper/pet/pet_mapper.dart';
 import 'package:flutter_lab/data/repositories/pet/pet_repository.dart';
+import 'package:flutter_lab/data/services/dio/petstore_client.dart' as api;
 import 'package:flutter_lab/domain/models/pet/pet.dart';
 import 'package:petstore/petstore.dart' as petstore;
 import 'package:result_dart/result_dart.dart';
@@ -10,9 +11,12 @@ class PetRepositoryRemote implements PetRepository {
     basePathOverride: 'http://localhost:3000',
   ).getPetsApi();
   final PetMapper _petMapper = PetMapper();
+  final petStoreClient = api.PetStoreClient(
+    Dio(BaseOptions(baseUrl: 'http://localhost:3000')),
+  );
 
   @override
-  Future<Result<List<Pet>>> listPets() async {
+  AsyncResult<List<Pet>> listPets() async {
     try {
       final response = await _petsApi.petsGet(limit: 10);
       final petDtos = response.data?.toList() ?? [];
@@ -23,5 +27,13 @@ class PetRepositoryRemote implements PetRepository {
     } on DioException catch (e) {
       return e.toFailure();
     }
+  }
+
+  @override
+  AsyncResult<Unit> createPet(Pet pet) async {
+    final dto = _petMapper.convert<Pet, petstore.Pet>(pet);
+    await _petsApi.petsPost(pet: dto);
+
+    return const Success(unit);
   }
 }
