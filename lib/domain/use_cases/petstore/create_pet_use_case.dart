fc60516a60237239da72b36fd247874bd612ccd0
Masahiko Kobayashi <kobaryubi6@gmail.com>
2025-10-12 16:30:48 +0900

    feat: lab

---
+++ lib/domain/use_cases/petstore/create_pet_use_case.dart
@@ -0,0 +1,14 @@
+import 'package:flutter_lab/data/repositories/pet/pet_repository.dart';
+import 'package:flutter_lab/domain/models/pet/pet.dart';
+import 'package:result_dart/result_dart.dart';
+
+class CreatePetUseCase {
+  CreatePetUseCase({required PetRepository petRepository})
+    : _petRepository = petRepository;
+
+  final PetRepository _petRepository;
+
+  AsyncResult<Unit> call(Pet pet) async {
+    return _petRepository.createPet(pet);
+  }
+}
