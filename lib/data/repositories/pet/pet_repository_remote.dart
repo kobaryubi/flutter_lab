0baa9e9fe0bcc157f313a316dfe42c38b4e67110
Masahiko Kobayashi <kobaryubi6@gmail.com>
2025-10-12 16:41:45 +0900

    feat: lab

--- lib/data/repositories/pet/pet_repository_remote.dart
+++ lib/data/repositories/pet/pet_repository_remote.dart
@@ -7,9 +7,12 @@ import 'package:petstore/petstore.dart' as petstore;
 import 'package:result_dart/result_dart.dart';
 
 class PetRepositoryRemote implements PetRepository {
-  final petstore.PetsApi _petsApi = petstore.Petstore(
-    basePathOverride: 'http://localhost:3000',
-  ).getPetsApi();
+  PetRepositoryRemote({required Dio dio})
+    : _petsApi = petstore.Petstore(
+        dio: dio,
+      ).getPetsApi();
+
+  final petstore.PetsApi _petsApi;
   final PetMapper _petMapper = PetMapper();
   final petStoreClient = api.PetStoreClient(
     Dio(BaseOptions(baseUrl: 'http://localhost:3000')),
