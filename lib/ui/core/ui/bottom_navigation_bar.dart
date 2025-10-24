16b5a8f098a8a0f8c2441cf560587989a2364eb1
Masahiko Kobayashi <kobaryubi6@gmail.com>
2025-10-25 08:03:49 +0900

    feat: lab

--- lib/ui/core/ui/bottom_navigation_bar.dart
+++ lib/ui/core/ui/bottom_navigation_bar.dart
@@ -1,21 +1,32 @@
 import 'package:flutter/widgets.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_lab/ui/core/themes/colors.dart';
+import 'package:go_router/go_router.dart';
 
 class BottomNavigationBar extends StatelessWidget {
-  const BottomNavigationBar({super.key});
+  const BottomNavigationBar({required this.navigationShell, super.key});
+
+  final StatefulNavigationShell navigationShell;
 
   @override
   Widget build(BuildContext context) {
     return Container(
       color: AppColors.primary,
       height: 56,
-      child: const Row(
+      child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
-          _Nav(label: 'Workouts'),
-          _Nav(label: 'Chat'),
-          _Nav(label: 'Profile'),
+          _Nav(label: 'Workouts', navigationShell: navigationShell, index: 0),
+          _Nav(
+            label: 'Chat',
+            navigationShell: navigationShell,
+            index: 1,
+          ),
+          _Nav(
+            label: 'Profile',
+            navigationShell: navigationShell,
+            index: 2,
+          ),
         ],
       ),
     );
@@ -23,13 +34,21 @@ class BottomNavigationBar extends StatelessWidget {
 }
 
 class _Nav extends HookWidget {
-  const _Nav({required this.label});
+  const _Nav({
+    required this.label,
+    required this.navigationShell,
+    required this.index,
+  });
 
   final String label;
+  final StatefulNavigationShell navigationShell;
+  final int index;
 
   @override
   Widget build(BuildContext context) {
-    final onTapped = useCallback(() {}, []);
+    final onTapped = useCallback(() {
+      navigationShell.goBranch(index);
+    }, []);
 
     return GestureDetector(
       onTap: onTapped,
