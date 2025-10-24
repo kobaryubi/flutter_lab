8c16fe62e9e526d44f1b45caaf0adc80542030c1
Masahiko Kobayashi <kobaryubi6@gmail.com>
2025-10-25 07:40:50 +0900

    feat: lab

---
+++ lib/ui/core/ui/bottom_navigation_bar.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/widgets.dart';
+
+class BottomNavigationBar extends StatelessWidget {
+  const BottomNavigationBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const SizedBox(height: 56, child: Row());
+  }
+}
