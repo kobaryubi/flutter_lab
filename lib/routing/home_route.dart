78b4f9b0db450d44ac9f3ff00399b65d3d296004
Masahiko Kobayashi <kobaryubi6@gmail.com>
2025-10-25 21:06:25 +0900

    feat: lab

--- lib/routing/home_route.dart
+++ lib/routing/home_route.dart
@@ -1,9 +1,4 @@
-import 'package:flutter/widgets.dart';
-import 'package:flutter_lab/routing/routes.dart';
-import 'package:flutter_lab/ui/home/widgets/home_screen.dart';
-import 'package:go_router/go_router.dart';
-
-part 'home_route.g.dart';
+part of 'router.dart';
 
 @TypedGoRoute<HomeRoute>(
   path: Routes.home,
