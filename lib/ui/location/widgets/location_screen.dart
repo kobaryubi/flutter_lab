8eb8be86ccd26e86b71145020db960b5bd52df81
Masahiko Kobayashi <kobaryubi6@gmail.com>
2025-10-26 16:11:56 +0900

    feat: lab

---
+++ lib/ui/location/widgets/location_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/widgets.dart';
+
+class LocationScreen extends StatelessWidget {
+  const LocationScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Location Screen'),
+    );
+  }
+}
