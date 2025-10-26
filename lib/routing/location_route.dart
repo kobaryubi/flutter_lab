6614b1ceb0688cec76a215e8a547a68f4dd610fd
Masahiko Kobayashi <kobaryubi6@gmail.com>
2025-10-26 16:23:11 +0900

    feat: lab

--- lib/routing/location_route.dart
+++ lib/routing/location_route.dart
@@ -7,6 +7,19 @@ part of 'router.dart';
 class LocationRoute extends GoRouteData with $LocationRoute {
   @override
   Widget build(BuildContext context, GoRouterState state) {
-    return const LocationScreen();
+    return ColoredBox(
+      color: AppColors.primary,
+      child: SafeArea(
+        child: ColoredBox(
+          color: AppColors.white1,
+          child: Column(
+            children: [
+              AppBar(title: const Text('Location')),
+              Expanded(child: const LocationScreen()),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
