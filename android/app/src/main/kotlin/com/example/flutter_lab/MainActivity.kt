eae11b6057168679847b985da813297e451499d2
Masahiko Kobayashi <kobaryubi6@gmail.com>
2025-11-04 20:06:42 +0900

    feat: lab

--- android/app/src/main/kotlin/com/example/flutter_lab/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/flutter_lab/MainActivity.kt
@@ -1,7 +1,23 @@
 package com.example.flutter_lab
 
+import androidx.annotation.NonNull
+import com.example.flutter_lab.infrastructure.platform.MethodChannelNames
+import com.example.flutter_lab.infrastructure.platform.MethodNames
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
 
 class MainActivity : FlutterActivity() {
-
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, MethodChannelNames.LOCATION).setMethodCallHandler { call, result ->
+            if (call.method == MethodNames.GET_LOCATION) {
+                val location = mapOf(
+                    "latitude" to 35.6895,
+                    "longitude" to 139.6917
+                )
+                result.success(location)
+            }
+        }
+    }
 }
---
+++ android/app/src/main/kotlin/com/example/flutter_lab/infrastructure/platform/MethodNames.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_lab.infrastructure.platform
+
+object MethodNames {
+    const val GET_BATTERY_LEVEL = "getBatteryLevel"
+    const val GET_LOCATION = "getLocation"
+}
