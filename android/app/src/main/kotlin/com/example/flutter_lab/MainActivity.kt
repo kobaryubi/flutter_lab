4849351bdecab9fd8b5d92094e4954403fd13e55
Masahiko Kobayashi <kobaryubi6@gmail.com>
2025-11-04 09:28:26 +0900

    feat: lab

--- android/app/src/main/kotlin/com/example/flutter_lab/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/flutter_lab/MainActivity.kt
@@ -2,4 +2,6 @@ package com.example.flutter_lab
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterActivity() {
+
+}
---
+++ android/app/src/main/kotlin/com/example/flutter_lab/infrastructure/platform/MethodChannelNames.kt
@@ -0,0 +1,7 @@
+package com.example.flutter_lab.infrastructure.platform
+
+object MethodChannelNames {
+    private const val BASE = "com.masahikokobayashi"
+    const val LOCATION = "$BASE/location"
+    const val BATTERY = "$BASE/battery"
+}
