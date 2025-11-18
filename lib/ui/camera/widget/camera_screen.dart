7feed0cc690b4532fdc4656fefdf555d687246be
Masahiko Kobayashi <kobaryubi6@gmail.com>
2025-11-19 08:13:34 +0900

    feat: lab

--- lib/ui/camera/widget/camera_screen.dart
+++ lib/ui/camera/widget/camera_screen.dart
@@ -1,4 +1,7 @@
+import 'package:camera/camera.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:flutter_lab/hook/use_camera_manager.dart';
 import 'package:flutter_lab/ui/core/ui/app_bar.dart';
 import 'package:flutter_lab/ui/core/ui/layout.dart';
 
@@ -7,9 +10,35 @@ class CameraScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Layout(
-      appBar: AppBar(title: Text('camera')),
-      child: Column(),
+    return Layout(
+      appBar: const AppBar(title: Text('camera')),
+      child: Column(children: [Expanded(child: _Body())]),
+    );
+  }
+}
+
+class _Body extends HookWidget {
+  @override
+  Widget build(BuildContext context) {
+    final cameraManager = useCameraManager();
+
+    final controller = cameraManager.cameraController;
+
+    if (cameraManager.camera == null || controller == null) {
+      return const Center(
+        child: Text('no camera found'),
+      );
+    }
+
+    if (cameraManager.isInitializedSnapshot.connectionState ==
+        ConnectionState.waiting) {
+      return const Center(
+        child: Text('initializing camera...'),
+      );
+    }
+
+    return Center(
+      child: CameraPreview(controller),
     );
   }
 }
