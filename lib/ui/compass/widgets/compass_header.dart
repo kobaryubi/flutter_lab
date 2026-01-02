379559a2858a50b2c722f77ec5e5de9997c67603
Masahiko Kobayashi <kobaryubi6@gmail.com>
2026-01-02 10:05:30 +0900

    🔥 remove _Title widget and logout view model

--- lib/ui/compass/widgets/compass_header.dart
+++ lib/ui/compass/widgets/compass_header.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/widgets.dart';
-import 'package:flutter_lab/l10n/app_localizations.dart';
 import 'package:flutter_lab/ui/compass/view_models/compass_view_model.dart';
 import 'package:flutter_lab/ui/core/themes/dimens.dart';
-import 'package:flutter_lab/ui/core/themes/theme.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 class CompassHeader extends StatelessWidget {
   const CompassHeader({required this.viewModel, super.key});
@@ -12,50 +9,16 @@ class CompassHeader extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final appLocalizations = AppLocalizations.of(context);
-    if (appLocalizations == null) {
-      return const Center(child: Text('Localization not found'));
-    }
-
     final user = viewModel.user;
     if (user == null) {
       return const SizedBox();
     }
 
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: [
-        ClipOval(
-          child: Image.asset(
-            user.picture,
-            width: Dimens.profilePictureSize,
-            height: Dimens.profilePictureSize,
-          ),
-        ),
-        const SizedBox(height: Dimens.paddingVertical),
-        _Title(text: appLocalizations.nameTrips(user.name)),
-      ],
-    );
-  }
-}
-
-class _Title extends StatelessWidget {
-  const _Title({required this.text});
-
-  final String text;
-
-  @override
-  Widget build(BuildContext context) {
-    return ShaderMask(
-      blendMode: BlendMode.srcIn,
-      shaderCallback: (Rect bounds) => const RadialGradient(
-        center: Alignment.bottomLeft,
-        radius: 2,
-        colors: [Color(0xFF7B1FA2), Color(0xFFAB47BC)],
-      ).createShader(Rect.fromLTWH(0, 0, bounds.width, bounds.height)),
-      child: Text(
-        text,
-        style: GoogleFonts.rubik(textStyle: TextStyles.headlineLarge),
+    return ClipOval(
+      child: Image.asset(
+        user.picture,
+        width: Dimens.profilePictureSize,
+        height: Dimens.profilePictureSize,
       ),
     );
   }
