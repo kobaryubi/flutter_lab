bbff7f854733cb6e84559252f6d25fd58c1df687
Masahiko Kobayashi <kobaryubi6@gmail.com>
2026-01-02 18:09:29 +0900

    🔥 remove ItineraryConfigRepository

--- lib/data/repositories/itinerary_config/itinerary_config_repository.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter_lab/domain/models/itinerary_config/itinerary_config.dart';
-import 'package:flutter_lab/utils/result.dart';
-
-abstract class ItineraryConfigRepository {
-  Future<Result<ItineraryConfig>> getItineraryConfig();
-  Future<Result<bool>> setItineraryConfig(ItineraryConfig itineraryConfig);
-}
