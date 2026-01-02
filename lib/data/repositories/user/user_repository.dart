6d8814aa14f014b25ca92d1cb6ddfbbf92f05992
Masahiko Kobayashi <kobaryubi6@gmail.com>
2026-01-02 10:44:44 +0900

    🔥 remove getUser method from UserRepository

--- lib/data/repositories/user/user_repository.dart
+++ lib/data/repositories/user/user_repository.dart
@@ -1,6 +1,1 @@
-import 'package:flutter_lab/domain/models/user/user.dart';
-import 'package:flutter_lab/utils/result.dart';
-
-abstract class UserRepository {
-  Future<Result<User>> getUser();
-}
+abstract class UserRepository {}
--- lib/data/repositories/user/user_repository_remote.dart
+++ lib/data/repositories/user/user_repository_remote.dart
@@ -1,33 +1,3 @@
 import 'package:flutter_lab/data/repositories/user/user_repository.dart';
-import 'package:flutter_lab/data/services/api/api_client.dart';
-import 'package:flutter_lab/data/services/api/model/user/user_api_model.dart';
-import 'package:flutter_lab/domain/models/user/user.dart';
-import 'package:flutter_lab/utils/result.dart';
 
-class UserRepositoryRemote implements UserRepository {
-  UserRepositoryRemote({required ApiClient apiClient}) : _apiClient = apiClient;
-
-  final ApiClient _apiClient;
-
-  User? _cachedData;
-
-  @override
-  Future<Result<User>> getUser() async {
-    if (_cachedData != null) {
-      return Future.value(Result.ok(_cachedData!));
-    }
-
-    final result = await _apiClient.getUser();
-    switch (result) {
-      case Ok<UserApiModel>():
-        final user = User(
-          name: result.value.name,
-          picture: result.value.picture,
-        );
-        _cachedData = user;
-        return Result.ok(user);
-      case Error<UserApiModel>():
-        return Result.error(result.error);
-    }
-  }
-}
+class UserRepositoryRemote implements UserRepository {}
