45499972c01b25d52f7c7f02dbd06489b6169285
Masahiko Kobayashi <kobaryubi6@gmail.com>
2026-01-04 08:56:35 +0900

    ♻️ use AsyncResult in agreement repository

--- lib/data/repositories/agreement/agreement_repository.dart
+++ lib/data/repositories/agreement/agreement_repository.dart
@@ -1,8 +1,10 @@
+import 'package:result_dart/result_dart.dart';
+
 /// Repository for managing agreement dates.
 abstract class AgreementRepository {
-  /// Gets the latest agreed date.
-  Future<DateTime?> getLatestAgreedDate();
+  /// Gets the latest agreed date. Returns Failure if not found.
+  AsyncResult<DateTime> getLatestAgreedDate();
 
   /// Saves the latest agreed date.
-  Future<void> saveLatestAgreedDate({required DateTime date});
+  AsyncResult<Unit> saveLatestAgreedDate({required DateTime date});
 }
