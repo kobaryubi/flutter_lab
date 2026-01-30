264194d6ac716ee0dfddb9830fb2d04157be5e96
Masahiko Kobayashi <kobaryubi6@gmail.com>
2026-01-31 08:11:56 +0900

    ✨ add ShortcutIconGateway interface

---
+++ lib/application/gateway/shortcut_icon_gateway.dart
@@ -0,0 +1,15 @@
+import 'dart:io';
+
+import 'package:result_dart/result_dart.dart';
+
+/// Gateway abstracting file system operations for shortcut icons.
+abstract class ShortcutIconGateway {
+  /// Saves an asset to the local file system.
+  AsyncResult<File> saveIcon({
+    required String assetPath,
+    required String fileName,
+  });
+
+  /// Deletes a local icon file.
+  AsyncResult<Unit> deleteIcon({required String fileName});
+}
