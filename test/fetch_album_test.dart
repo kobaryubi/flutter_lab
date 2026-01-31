d6f00de9d25afe35863a0d22d9e54e9135daeb20
Masahiko Kobayashi <kobaryubi6@gmail.com>
2026-01-31 10:13:00 +0900

    ♻️ migrate fetch_album_test from mockito to mocktail

--- test/fetch_album_test.dart
+++ test/fetch_album_test.dart
@@ -1,24 +1,24 @@
 import 'package:flutter_lab/domain/models/album/album.dart';
 import 'package:flutter_lab/ui/cookbook/widgets/cookbook_networking_fetch_data_screen.dart';
 import 'package:flutter_test/flutter_test.dart';
-// import 'package:mockito/mockito.dart';
-
-// import 'fetch_album_test.mocks.dart';
-
 import 'package:http/http.dart' as http;
-import 'package:mockito/annotations.dart';
-import 'package:mockito/mockito.dart';
+import 'package:mocktail/mocktail.dart';
 
-import 'fetch_album_test.mocks.dart';
+class MockClient extends Mock implements http.Client {}
 
-@GenerateMocks([http.Client])
 void main() {
+  setUpAll(() {
+    registerFallbackValue(Uri.parse('https://example.com'));
+  });
+
   group('fetchAlbum', () {
     test('returns an Album if the http call completes successfully', () async {
       final client = MockClient();
 
       when(
-        client.get(Uri.parse('https://jsonplaceholder.typicode.com/albums/1')),
+        () => client.get(
+          Uri.parse('https://jsonplaceholder.typicode.com/albums/1'),
+        ),
       ).thenAnswer(
         (_) async =>
             http.Response('{"userId": 1, "id": 2, "title": "mock"}', 200),
@@ -31,7 +31,9 @@ void main() {
       final client = MockClient();
 
       when(
-        client.get(Uri.parse('https://jsonplaceholder.typicode.com/albums/1')),
+        () => client.get(
+          Uri.parse('https://jsonplaceholder.typicode.com/albums/1'),
+        ),
       ).thenAnswer((_) async => http.Response('Not Found', 404));
 
       expect(fetchAlbum(client), throwsException);
