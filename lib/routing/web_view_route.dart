d20794152379cd5444f1b481689c0a377a86b3f7
Masahiko Kobayashi <kobaryubi6@gmail.com>
2026-02-02 09:03:16 +0900

    ✨ add URL parameter to WebViewRoute

--- lib/routing/web_view_route.dart
+++ lib/routing/web_view_route.dart
@@ -3,8 +3,13 @@ part of 'router.dart';
 @TypedGoRoute<WebViewRoute>(path: Routes.webView)
 @immutable
 class WebViewRoute extends GoRouteData with $WebViewRoute {
+  const WebViewRoute({required this.url});
+
+  /// The URL to load in the web view.
+  final String url;
+
   @override
   Widget build(BuildContext context, GoRouterState state) {
-    return const WebViewScreen();
+    return WebViewScreen(url: url);
   }
 }
