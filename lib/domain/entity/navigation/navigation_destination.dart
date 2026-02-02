440f041d77130cbb8b7c6bc8bd662792bf3d1ff3
Masahiko Kobayashi <kobaryubi6@gmail.com>
2026-02-02 09:44:08 +0900

    ✨ add NavigationDestination enum

---
+++ lib/domain/entity/navigation/navigation_destination.dart
@@ -0,0 +1,8 @@
+/// Represents where a URL should be opened.
+enum NavigationDestination {
+  /// Open the URL in an in-app WebView.
+  webView,
+
+  /// Open the URL in the device's external browser.
+  externalBrowser,
+}
