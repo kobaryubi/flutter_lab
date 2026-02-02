12049ca765d299240d1f34aa71ecce0128020c73
Masahiko Kobayashi <kobaryubi6@gmail.com>
2026-02-02 15:31:09 +0900

    ✨ add LocalIconViewModel and DeleteAllShortcutIconsUseCase

---
+++ lib/domain/use_cases/shortcut/delete_all_shortcut_icons_use_case.dart
@@ -0,0 +1,16 @@
+import 'package:flutter_lab/domain/shortcut/shortcut_repository.dart';
+import 'package:result_dart/result_dart.dart';
+
+/// Deletes all shortcut icons from the local file system.
+class DeleteAllShortcutIconsUseCase {
+  DeleteAllShortcutIconsUseCase({
+    required ShortcutRepository shortcutRepository,
+  }) : _shortcutRepository = shortcutRepository;
+
+  final ShortcutRepository _shortcutRepository;
+
+  /// Removes every shortcut icon by delegating to the repository.
+  AsyncResult<Unit> call() async {
+    return _shortcutRepository.deleteAllShortcutIcons();
+  }
+}
---
+++ lib/ui/local_icon/view_model/local_icon_view_model.dart
@@ -0,0 +1,32 @@
+import 'dart:io';
+
+import 'package:flutter_lab/application/di/provider.dart';
+import 'package:flutter_lab/ui/local_icon/ui_state/local_icon_ui_state.dart';
+import 'package:result_dart/result_dart.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart' hide AsyncResult;
+
+part 'local_icon_view_model.g.dart';
+
+/// ViewModel that manages copying and deleting shortcut icons.
+@riverpod
+class LocalIconViewModel extends _$LocalIconViewModel {
+  @override
+  LocalIconUiState build() {
+    return const LocalIconUiState();
+  }
+
+  /// Copies the bundled asset to the local file system.
+  AsyncResult<File> copyAsset() async {
+    final repository = ref.read(shortcutRepositoryProvider);
+    return repository.saveShortcutIcon(
+      assetPath: 'assets/images/search.png',
+      fileName: 'search.png',
+    );
+  }
+
+  /// Deletes all shortcut icons.
+  AsyncResult<Unit> deleteAllShortcutIcons() async {
+    final useCase = ref.read(deleteAllShortcutIconsUseCaseProvider);
+    return useCase.call();
+  }
+}
