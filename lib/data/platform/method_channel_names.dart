6e0b3942f0218169276e2a408c5848b6713839cd
Masahiko Kobayashi <kobaryubi6@gmail.com>
2026-02-02 09:20:35 +0900

    ✨ add arutanaAd method channel name

--- lib/data/platform/method_channel_names.dart
+++ lib/data/platform/method_channel_names.dart
@@ -2,4 +2,5 @@ class MethodChannelNames {
   static const _base = 'com.masahikokobayashi';
   static const location = '$_base/location';
   static const battery = '$_base/battery';
+  static const arutanaAd = '$_base/arutana_ad';
 }
