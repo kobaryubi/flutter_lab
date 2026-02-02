bb084270582c7f2ded2325d412232c83c792a6e4
Masahiko Kobayashi <kobaryubi6@gmail.com>
2026-02-02 10:05:31 +0900

    ♻️ move urlNavigationList from mixin getter to determine parameter

--- lib/ui/url_navigation/view_model/url_navigation_view_model.dart
+++ lib/ui/url_navigation/view_model/url_navigation_view_model.dart
@@ -1,4 +1,4 @@
-import 'package:flutter_lab/domain/entity/navigation/url_navigation_list.dart';
+import 'package:flutter_lab/domain/entity/navigation/navigation_destination.dart';
 import 'package:flutter_lab/presentation/core/mixin/url_navigation_mixin.dart';
 import 'package:flutter_lab/ui/url_navigation/ui_state/url_navigation_ui_state.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
@@ -14,13 +14,18 @@ class UrlNavigationViewModel extends _$UrlNavigationViewModel
     return const UrlNavigationUiState(urlNavigationList: .loading());
   }
 
-  @override
-  UrlNavigationList? get urlNavigationList => state.urlNavigationList.value;
-
   /// Fetches the navigation list using [UrlNavigationMixin].
   Future<void> fetch() async {
     state = state.copyWith(
       urlNavigationList: await AsyncValue.guard(fetchUrlNavigationList),
+    );
+  }
+
+  /// Determines the navigation destination for the given [url].
+  NavigationDestination determineDestination({required String url}) {
+    return determine(
+      url: url,
+      urlNavigationList: state.urlNavigationList.value,
     );
   }
 }
