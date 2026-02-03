7209010a5332a3a657b56a56c6bdcc354b040c2d
Masahiko Kobayashi <kobaryubi6@gmail.com>
2026-02-03 21:14:14 +0900

    🔥 remove SubscriptionRepository from dependencies

--- lib/config/dependencies.dart
+++ lib/config/dependencies.dart
@@ -1,22 +1,10 @@
-import 'package:flutter/widgets.dart';
 import 'package:flutter_lab/data/repositories/auth/auth_repository.dart';
 import 'package:flutter_lab/data/repositories/auth/auth_repository_dev.dart';
-import 'package:flutter_lab/data/repositories/subscription/subscription_repository.dart';
-import 'package:flutter_lab/data/repositories/subscription/subscription_repository_local.dart';
 import 'package:provider/provider.dart';
 import 'package:provider/single_child_widget.dart';
 
-List<SingleChildWidget> _sharedProviders = [
-  Provider<SubscriptionRepository>(
-    create: (BuildContext context) => SubscriptionRepositoryLocal(),
-  ),
-];
-
-final List<SingleChildWidget> providersRemote = [
-  ..._sharedProviders,
-];
+final List<SingleChildWidget> providersRemote = [];
 
 final List<SingleChildWidget> providersLocal = [
   ChangeNotifierProvider<AuthRepository>(create: (_) => AuthRepositoryDev()),
-  ..._sharedProviders,
 ];
