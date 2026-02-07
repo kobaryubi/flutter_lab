cc19730958fe6695552aafee1af3cef81634f623
Masahiko Kobayashi <kobaryubi6@gmail.com>
2026-02-08 08:13:12 +0900

    🔥 remove MultiProvider and guard Firebase init for iOS only

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,25 @@
+import 'dart:io';
+
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_lab/application/di/provider.dart';
-import 'package:flutter_lab/config/dependencies.dart';
 import 'package:flutter_lab/domain/location/platform_location_repository.dart';
 import 'package:flutter_lab/firebase_options.dart';
 import 'package:flutter_lab/flavors.dart';
 import 'package:flutter_lab/flutter_lab_app.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:logging/logging.dart';
-import 'package:provider/provider.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+  // TODO(masahiko): Configure Firebase for Android and remove this guard.
+  if (Platform.isIOS) {
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+  }
 
   Logger.root.level = Level.ALL;
 
@@ -33,10 +36,7 @@ Future<void> main() async {
           PlatformLocationRepository(),
         ),
       ],
-      child: MultiProvider(
-        providers: providersLocal,
-        child: const FlutterLabApp(),
-      ),
+      child: const FlutterLabApp(),
     ),
   );
 }
