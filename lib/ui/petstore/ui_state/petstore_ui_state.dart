ce7c33fde4c603b343baefc4d8995af5993193db
Masahiko Kobayashi <kobaryubi6@gmail.com>
2026-02-08 08:23:42 +0900

    🔥 remove petstore UI state and ViewModel

--- lib/ui/petstore/ui_state/petstore_ui_state.dart
+++
@@ -1,8 +0,0 @@
-import 'package:freezed_annotation/freezed_annotation.dart';
-
-part 'petstore_ui_state.freezed.dart';
-
-@freezed
-abstract class PetStoreUiState with _$PetStoreUiState {
-  const factory PetStoreUiState() = _PetStoreUiState;
-}
