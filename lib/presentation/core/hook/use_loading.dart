7060d5c44f294a5fe6b7440d77c4a239724b232b
Masahiko Kobayashi <kobaryubi6@gmail.com>
2026-02-08 15:53:31 +0900

    ♻️ refactor useLoading to use ref.listen and addPostFrameCallback

--- lib/presentation/core/hook/use_loading.dart
+++ lib/presentation/core/hook/use_loading.dart
@@ -1,25 +1,31 @@
 import 'package:flutter/widgets.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_lab/presentation/core/provider/global_loading_notifier.dart';
+import 'package:flutter_lab/ui/loading/view_model/loading_view_model.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-/// A hook that controls the global loading overlay based on [isLoading].
+/// A hook that manages the global loading overlay.
 ///
-/// Defers provider modifications to after the build phase using
-/// [WidgetsBinding.addPostFrameCallback].
-/// Automatically hides the loading overlay when the widget is disposed.
-void useLoading({required WidgetRef ref, required bool isLoading}) {
-  useEffect(
-    () {
+/// Listens to [loadingViewModelProvider] to show/hide the overlay,
+/// and ensures the overlay is hidden when the widget is disposed
+/// using [WidgetsBinding.addPostFrameCallback].
+void useLoading({required WidgetRef ref}) {
+  ref.listen(
+    loadingViewModelProvider.select(
+      (uiState) => uiState.data is AsyncLoading,
+    ),
+    (previous, isLoading) {
       final notifier = ref.read(globalLoadingProvider.notifier);
-      WidgetsBinding.instance.addPostFrameCallback(
-        (_) => isLoading ? notifier.show() : notifier.hide(),
-      );
-
-      return () => WidgetsBinding.instance.addPostFrameCallback(
-        (_) => notifier.hide(),
-      );
+      isLoading ? notifier.show() : notifier.hide();
     },
-    [isLoading],
+  );
+
+  useEffect(
+    () => () {
+      WidgetsBinding.instance.addPostFrameCallback((_) {
+        ref.read(globalLoadingProvider.notifier).hide();
+      });
+    },
+    const [],
   );
 }
