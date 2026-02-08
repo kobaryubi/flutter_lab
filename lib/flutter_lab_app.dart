c90818220f7e3b6581ec69ea42b55930f6d8945a
Masahiko Kobayashi <kobaryubi6@gmail.com>
2026-02-09 07:07:22 +0900

    ✨ add FlutterLabAppViewModel to listen for token refresh events

--- lib/flutter_lab_app.dart
+++ lib/flutter_lab_app.dart
@@ -4,6 +4,7 @@ import 'package:flutter_lab/presentation/core/widget/global_error_overlay.dart';
 import 'package:flutter_lab/routing/router.dart';
 import 'package:flutter_lab/ui/core/themes/colors.dart';
 import 'package:flutter_lab/ui/core/themes/theme.dart';
+import 'package:flutter_lab/ui/flutter_lab_app/view_model/flutter_lab_app_view_model.dart';
 import 'package:flutter_portal/flutter_portal.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
@@ -13,6 +14,7 @@ class FlutterLabApp extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final router = ref.watch(routerProvider);
+    ref.listen(flutterLabAppViewModelProvider, (previous, next) {});
 
     return Portal(
       child: WidgetsApp.router(
@@ -24,9 +26,9 @@ class FlutterLabApp extends ConsumerWidget {
         textStyle: TextStyles.bodyMedium.merge(
           const TextStyle(color: AppColors.black1),
         ),
-        builder: (context, child) {
-          return GlobalErrorOverlay(child: child ?? const SizedBox.shrink());
-        },
+        builder: (context, child) => GlobalErrorOverlay(
+          child: child ?? const SizedBox.shrink(),
+        ),
       ),
     );
   }
