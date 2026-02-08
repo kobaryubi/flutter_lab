17885d8f795a20f9618f4666223449885fdb7d2b
Masahiko Kobayashi <kobaryubi6@gmail.com>
2026-02-08 14:53:49 +0900

    🐛 fix provider modification during build in useLoading hook

--- lib/presentation/core/hook/use_loading.dart
+++ lib/presentation/core/hook/use_loading.dart
@@ -1,16 +1,24 @@
+import 'package:flutter/widgets.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_lab/presentation/core/provider/global_loading_notifier.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 /// A hook that controls the global loading overlay based on [isLoading].
 ///
+/// Defers provider modifications to after the build phase using
+/// [WidgetsBinding.addPostFrameCallback].
 /// Automatically hides the loading overlay when the widget is disposed.
 void useLoading({required WidgetRef ref, required bool isLoading}) {
   useEffect(
     () {
       final notifier = ref.read(globalLoadingProvider.notifier);
-      isLoading ? notifier.show() : notifier.hide();
-      return notifier.hide;
+      WidgetsBinding.instance.addPostFrameCallback(
+        (_) => isLoading ? notifier.show() : notifier.hide(),
+      );
+
+      return () => WidgetsBinding.instance.addPostFrameCallback(
+        (_) => notifier.hide(),
+      );
     },
     [isLoading],
   );
