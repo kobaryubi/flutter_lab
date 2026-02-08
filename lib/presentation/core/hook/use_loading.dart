dbde0551adc12d3f1420a1ff1f59b820a6fc1d6e
Masahiko Kobayashi <kobaryubi6@gmail.com>
2026-02-08 16:26:02 +0900

    🐛 fix StateError in useLoading by checking context.mounted on dispose

--- lib/presentation/core/hook/use_loading.dart
+++ lib/presentation/core/hook/use_loading.dart
@@ -6,9 +6,11 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 /// A hook that hides the global loading overlay when the widget is disposed
 /// using [WidgetsBinding.addPostFrameCallback].
 void useLoading({required WidgetRef ref}) {
+  final context = useContext();
   useEffect(
     () => () {
       WidgetsBinding.instance.addPostFrameCallback((_) {
+        if (!context.mounted) return;
         ref.read(globalLoadingProvider.notifier).hide();
       });
     },
