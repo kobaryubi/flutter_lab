93354d645981ede864044a6d4ba3450e9a20c815
Masahiko Kobayashi <kobaryubi6@gmail.com>
2026-02-10 08:33:54 +0900

    ♻️ rename LocationRepository.get() to getLocation()

--- lib/domain/location/location_repository.dart
+++ lib/domain/location/location_repository.dart
@@ -2,5 +2,5 @@ import 'package:flutter_lab/domain/location/location.dart';
 import 'package:result_dart/result_dart.dart';
 
 abstract class LocationRepository {
-  AsyncResult<Location> get();
+  AsyncResult<Location> getLocation();
 }
