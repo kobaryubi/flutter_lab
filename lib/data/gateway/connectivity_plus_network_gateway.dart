c2699ff4faf20be2e33e7494e137cbc90901279a
Masahiko Kobayashi <kobaryubi6@gmail.com>
2026-02-11 11:27:27 +0900

    ✨ add connectivity change log to ConnectivityPlusNetworkGateway

--- lib/data/gateway/connectivity_plus_network_gateway.dart
+++ lib/data/gateway/connectivity_plus_network_gateway.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter_lab/domain/entity/network/connectivity_status.dart';
 import 'package:flutter_lab/domain/network/network_gateway.dart';
@@ -16,6 +18,7 @@ class ConnectivityPlusNetworkGateway implements NetworkGateway {
 
   ConnectivityStatus _toConnectivityStatus(List<ConnectivityResult> results) {
     final connectivities = results.map((result) => result.name).toList();
+    log('Connectivity changed: $connectivities');
     return ConnectivityStatus(connectivities: connectivities);
   }
 }
