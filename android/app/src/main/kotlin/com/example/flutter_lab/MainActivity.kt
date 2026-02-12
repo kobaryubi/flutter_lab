af254a5a7af11dadd87f521743400c4fd6789e40
Masahiko Kobayashi <kobaryubi6@gmail.com>
2026-02-12 09:28:53 +0900

    ✨ add empty location method channel handler in MainActivity

--- android/app/src/main/kotlin/com/example/flutter_lab/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/flutter_lab/MainActivity.kt
@@ -2,9 +2,25 @@ package com.example.flutter_lab
 
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
 
 class MainActivity : FlutterActivity() {
+    companion object {
+        private const val LOCATION_CHANNEL = "com.masahikokobayashi/location"
+    }
+
     override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
         super.configureFlutterEngine(flutterEngine)
+
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, LOCATION_CHANNEL)
+            .setMethodCallHandler { call, result ->
+                when (call.method) {
+                    "getLocation" -> {
+                        // TODO: Implement location retrieval
+                        result.notImplemented()
+                    }
+                    else -> result.notImplemented()
+                }
+            }
     }
 }
