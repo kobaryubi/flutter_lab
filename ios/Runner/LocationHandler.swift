85006bc78374a97f16267b812ab80c419437b5e6
Masahiko Kobayashi <kobaryubi6@gmail.com>
2026-02-13 16:25:42 +0900

    ✨ add EventChannel location watching to iOS LocationHandler

--- ios/Runner/LocationHandler.swift
+++ ios/Runner/LocationHandler.swift
@@ -9,6 +9,7 @@ final class LocationHandler: NSObject {
   
   private enum MethodName {
     static let getLocation = "getLocation"
+    static let watchLocation = "watchLocation"
   }
   
   private enum ErrorCode {
@@ -19,12 +20,18 @@ final class LocationHandler: NSObject {
   // MARK: - Properties
   
   private let methodChannel: FlutterMethodChannel
+  private let eventChannel: FlutterEventChannel
   private let locationManager: CLLocationManager
+  private var eventSink: FlutterEventSink?
   
   // MARK: - Initialization
   
   init(binaryMessenger: any FlutterBinaryMessenger) {
     self.methodChannel = FlutterMethodChannel(
+      name: Self.channelName,
+      binaryMessenger: binaryMessenger
+    )
+    self.eventChannel = FlutterEventChannel(
       name: Self.channelName,
       binaryMessenger: binaryMessenger
     )
@@ -34,6 +41,7 @@ final class LocationHandler: NSObject {
     
     locationManager.delegate = self
     methodChannel.setMethodCallHandler(handleMethodCall)
+    eventChannel.setStreamHandler(self)
   }
   
   // MARK: - Method Call Handler
@@ -45,6 +53,9 @@ final class LocationHandler: NSObject {
     switch call.method {
     case MethodName.getLocation:
       getLocation(result: result)
+    case MethodName.watchLocation:
+      watchLocation()
+      result(nil)
     default:
       result(FlutterMethodNotImplemented)
     }
@@ -80,8 +91,46 @@ final class LocationHandler: NSObject {
     
     result(locationData)
   }
+  
+  private func watchLocation() {
+    locationManager.startUpdatingLocation()
+  }
 }
 
 // MARK: - CLLocationManagerDelegate
 
-extension LocationHandler: CLLocationManagerDelegate {}
+extension LocationHandler: CLLocationManagerDelegate {
+  func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+    guard let location = locations.last else { return }
+    
+    let locationData: [String: Double] = [
+      "latitude": location.coordinate.latitude,
+      "longitude": location.coordinate.longitude
+    ]
+    
+    eventSink?(locationData)
+  }
+  
+  func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+    eventSink?(FlutterError(
+      code: ErrorCode.unavailable,
+      message: "Location update failed: \(error.localizedDescription)",
+      details: nil
+    ))
+  }
+}
+// MARK: - FlutterStreamHandler
+
+extension LocationHandler: FlutterStreamHandler {
+  func onListen(withArguments arguments: Any?, eventSink events: @escaping FlutterEventSink) -> FlutterError? {
+    self.eventSink = events
+    return nil
+  }
+  
+  func onCancel(withArguments arguments: Any?) -> FlutterError? {
+    locationManager.stopUpdatingLocation()
+    self.eventSink = nil
+    return nil
+  }
+}
+
