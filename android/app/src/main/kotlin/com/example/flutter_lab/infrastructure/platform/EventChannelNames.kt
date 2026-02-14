2eacfcbbbcd97235bb55dc39be29ab6996a8cd23
Masahiko Kobayashi <kobaryubi6@gmail.com>
2026-02-14 11:12:33 +0900

    ✨ add EventChannelNames constants for Android

---
+++ android/app/src/main/kotlin/com/example/flutter_lab/infrastructure/platform/EventChannelNames.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_lab.infrastructure.platform
+
+object EventChannelNames {
+    private const val BASE = "com.masahikokobayashi"
+    const val LOCATION_UPDATES = "$BASE/locationUpdates"
+}
