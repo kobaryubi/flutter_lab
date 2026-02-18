0ea3668985a9d7016d8583ec3d6d4092dda1756b
Masahiko Kobayashi <kobaryubi6@gmail.com>
2026-02-19 06:32:02 +0900

    ✨ add OpenAPI spec for ETag cache demo API

---
+++ package/etag_cache/lib/etag_cache.dart
@@ -0,0 +1,16 @@
+//
+// AUTO-GENERATED FILE, DO NOT MODIFY!
+//
+
+export 'package:etag_cache/src/api.dart';
+export 'package:etag_cache/src/auth/api_key_auth.dart';
+export 'package:etag_cache/src/auth/basic_auth.dart';
+export 'package:etag_cache/src/auth/bearer_auth.dart';
+export 'package:etag_cache/src/auth/oauth.dart';
+export 'package:etag_cache/src/serializers.dart';
+export 'package:etag_cache/src/model/date.dart';
+
+export 'package:etag_cache/src/api/products_api.dart';
+
+export 'package:etag_cache/src/model/product.dart';
+
