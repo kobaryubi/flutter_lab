08f586cf03695e6867c9fe50aede3c83c7915a6c
Masahiko Kobayashi <kobaryubi6@gmail.com>
2026-03-03 17:10:55 +0900

    🔥 remove greeting gateway and formatted greeting use case

--- lib/domain/greeting/greeting_gateway.dart
+++
@@ -1,7 +0,0 @@
-import 'package:result_dart/result_dart.dart';
-
-/// Gateway for loading greeting messages.
-abstract class GreetingGateway {
-  /// Loads a greeting message asynchronously.
-  AsyncResult<String> getGreeting();
-}
