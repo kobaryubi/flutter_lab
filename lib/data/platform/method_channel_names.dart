fabcd85e65d26a24d1c7f07acd8924d9327320ce
Masahiko Kobayashi <kobaryubi6@gmail.com>
2026-03-03 09:04:28 +0900

    🔥 remove arutana ad AAR and method channel constants

--- lib/data/platform/method_channel_names.dart
+++ lib/data/platform/method_channel_names.dart
@@ -2,7 +2,6 @@ class MethodChannelNames {
   static const _base = 'com.masahikokobayashi';
   static const location = '$_base/location';
   static const battery = '$_base/battery';
-  static const arutanaAd = '$_base/arutana_ad';
   static const nativeButton = '$_base/nativeButton';
   static const pigeon = '$_base/pigeon';
 }
--- lib/data/platform/method_names.dart
+++ lib/data/platform/method_names.dart
@@ -5,13 +5,4 @@ class MethodNames {
 
   // Pigeon - triggers native to call Dart via FlutterApi
   static const String callFlutterMethod = 'callFlutterMethod';
-
-  // Arutana Ad - commands (Flutter -> Native)
-  static const String showInterstitial = 'showInterstitial';
-
-  // Arutana Ad - callbacks (Native -> Flutter)
-  static const String onInterstitialReceived = 'onInterstitialReceived';
-  static const String onInterstitialShown = 'onInterstitialShown';
-  static const String onInterstitialClosed = 'onInterstitialClosed';
-  static const String onInterstitialFailed = 'onInterstitialFailed';
 }
