5b8dcb696b82520c74c40f375592c18ae021cc8f
Masahiko Kobayashi <kobaryubi6@gmail.com>
2026-03-12 05:50:43 +0900

    🔥 disable ProfilePassport SDK integration temporarily

--- android/app/src/main/kotlin/com/masahikokobayashi/flutterlab/pigeon/gateway/ProfilePassportHostApiImpl.kt
+++ android/app/src/main/kotlin/com/masahikokobayashi/flutterlab/pigeon/gateway/ProfilePassportHostApiImpl.kt
@@ -1,31 +1,32 @@
 package com.masahikokobayashi.flutterlab.pigeon.gateway
 
-import android.content.Context
-import android.util.Log
-import com.masahikokobayashi.flutterlab.pigeon.ProfilePassportHostApi
-import jp.co.ymds.profilepassport.sdk.PPSDKManager
-
-/// Implementation of [ProfilePassportHostApi] for ProfilePassport SDK integration.
-class ProfilePassportHostApiImpl(
-    private val context: Context
-) : ProfilePassportHostApi {
-
-    /// Starts the PPSDK service with default start options.
-    override fun startService(callback: (Result<Unit>) -> Unit) {
-        val startOption: HashMap<String, String> = HashMap()
-        startOption["isRequestLocationPermit"] = "false"
-        startOption["isRequestBackgroundLocationPermit"] = "false"
-        startOption["isRequestBluetoothPermit"] = "false"
-        startOption["isRequestPushPermit"] = "false"
-
-        PPSDKManager.sharedManager(context.applicationContext)
-            .serviceStart(context, startOption) { code, message ->
-                Log.d(TAG, "PPSDK service start result code:$code, message:$message")
-                callback(Result.success(Unit))
-            }
-    }
-
-    companion object {
-        private const val TAG = "ProfilePassportHostApiImpl"
-    }
-}
+// import android.content.Context
+// import android.util.Log
+// import com.masahikokobayashi.flutterlab.pigeon.ProfilePassportHostApi
+// import jp.co.ymds.profilepassport.sdk.PPSDKManager
+//
+// /// Implementation of [ProfilePassportHostApi] for ProfilePassport SDK integration.
+// class ProfilePassportHostApiImpl(
+//     private val context: Context
+// ) : ProfilePassportHostApi {
+//
+//     /// Starts the PPSDK service with default start options.
+//     override fun startService(callback: (Result<Unit>) -> Unit) {
+//         val startOption: HashMap<String, String> = HashMap()
+//         startOption["isRequestLocationPermit"] = "false"
+//         startOption["isRequestBackgroundLocationPermit"] = "false"
+//         startOption["isRequestBluetoothPermit"] = "false"
+//         startOption["isRequestPushPermit"] = "false"
+//         startOption["isRequestPushPermit"] = "false"
+//
+//         PPSDKManager.sharedManager(context.applicationContext)
+//             .serviceStart(context, startOption) { code, message ->
+//                 Log.d(TAG, "PPSDK service start result code:$code, message:$message")
+//                 callback(Result.success(Unit))
+//             }
+//     }
+//
+//     companion object {
+//         private const val TAG = "ProfilePassportHostApiImpl"
+//     }
+// }
