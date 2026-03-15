3d9f8b97d4cf26e1dcebd711930fedfcfbcc1bd2
Masahiko Kobayashi <kobaryubi6@gmail.com>
2026-03-15 20:28:58 +0900

    ✨ add LogTapEventUseCase for tap analytics logging

---
+++ lib/domain/use_cases/analytics/log_tap_event_use_case.dart
@@ -0,0 +1,21 @@
+import 'package:flutter_lab/domain/analytics/analytics_gateway.dart';
+import 'package:result_dart/result_dart.dart';
+
+/// Use case that logs a tap analytics event.
+///
+/// Encapsulates the event name and parameter structure
+/// so callers only need to specify the tap target.
+class LogTapEventUseCase {
+  LogTapEventUseCase({
+    required AnalyticsGateway analyticsGateway,
+  }) : _analyticsGateway = analyticsGateway;
+
+  final AnalyticsGateway _analyticsGateway;
+
+  /// Logs a tap event for the given [target].
+  AsyncResult<Unit> call({required String target}) =>
+      _analyticsGateway.logEvent(
+        name: 'tap',
+        parameters: {'target': target},
+      );
+}
