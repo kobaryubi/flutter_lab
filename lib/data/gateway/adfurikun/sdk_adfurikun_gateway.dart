9ce1ec1c0ab9d2297112e2c9fca3ff491a35b152
Masahiko Kobayashi <kobaryubi6@gmail.com>
2026-03-17 20:33:59 +0900

    ✨ add reward ad support to AdfurikunGateway

--- lib/data/gateway/adfurikun/sdk_adfurikun_gateway.dart
+++ lib/data/gateway/adfurikun/sdk_adfurikun_gateway.dart
@@ -1,14 +1,17 @@
 import 'dart:async';
 
 import 'package:adfurikunsdk/interstitial.dart';
+import 'package:adfurikunsdk/reward.dart';
 import 'package:flutter_lab/domain/adfurikun/adfurikun_gateway.dart';
 import 'package:result_dart/result_dart.dart';
 
 /// Adfurikun SDK implementation of [AdfurikunGateway].
 class SdkAdfurikunGateway implements AdfurikunGateway {
   final Map<String, AdfurikunInterstitial> _interstitialAds = {};
+  final Map<String, AdfurikunReward> _rewardAds = {};
   final Map<String, Completer<Unit>> _loadCompleters = {};
   final Map<String, Completer<Unit>> _showCompleters = {};
+  final Map<String, bool> _rewardEarnedMap = {};
 
   @override
   AsyncResult<Unit> initializeInterstitialAd({required String appId}) async {
@@ -29,10 +32,12 @@ class SdkAdfurikunGateway implements AdfurikunGateway {
     try {
       final interstitial = _getInterstitial(appId);
 
-      _loadCompleters[appId] = Completer<Unit>();
-      interstitial.load();
+      final completer = Completer<Unit>();
 
-      await _loadCompleters[appId]!.future;
+      _loadCompleters[appId] = completer;
+      interstitial.loadWithTimeout(10);
+
+      await completer.future;
 
       return const Success(unit);
     } on Exception catch (exception) {
@@ -51,16 +56,79 @@ class SdkAdfurikunGateway implements AdfurikunGateway {
         return Failure(Exception('Interstitial ad is not prepared: $appId'));
       }
 
-      _showCompleters[appId] = Completer<Unit>();
+      final completer = Completer<Unit>();
+
+      _showCompleters[appId] = completer;
       interstitial.play();
 
-      await _showCompleters[appId]!.future;
+      await completer.future;
 
       return const Success(unit);
     } on Exception catch (exception) {
       return Failure(exception);
     }
   }
+
+  @override
+  AsyncResult<Unit> initializeRewardAd({required String appId}) async {
+    try {
+      final reward = AdfurikunReward(appId)
+        ..listener = _createRewardListener(appId);
+
+      _rewardAds[appId] = reward;
+
+      return const Success(unit);
+    } on Exception catch (exception) {
+      return Failure(exception);
+    }
+  }
+
+  @override
+  AsyncResult<Unit> loadRewardAd({required String appId}) async {
+    try {
+      final reward = _getReward(appId);
+
+      final completer = Completer<Unit>();
+
+      _loadCompleters[appId] = completer;
+      reward.loadWithTimeout(10);
+
+      await completer.future;
+
+      return const Success(unit);
+    } on Exception catch (exception) {
+      return Failure(exception);
+    }
+  }
+
+  @override
+  AsyncResult<Unit> showRewardAd({required String appId}) async {
+    try {
+      final reward = _getReward(appId);
+
+      final isPrepared = await reward.isPrepared();
+
+      if (!isPrepared) {
+        return Failure(Exception('Reward ad is not prepared: $appId'));
+      }
+
+      final completer = Completer<Unit>();
+
+      _rewardEarnedMap[appId] = false;
+      _showCompleters[appId] = completer;
+      reward.play();
+
+      await completer.future;
+
+      return const Success(unit);
+    } on Exception catch (exception) {
+      return Failure(exception);
+    }
+  }
+
+  @override
+  bool isRewardEarned({required String appId}) =>
+      _rewardEarnedMap[appId] ?? false;
 
   @override
   void dispose() {
@@ -70,9 +138,17 @@ class SdkAdfurikunGateway implements AdfurikunGateway {
         ..listener = null;
     }
 
+    for (final reward in _rewardAds.values) {
+      reward
+        ..destroy()
+        ..listener = null;
+    }
+
     _interstitialAds.clear();
+    _rewardAds.clear();
     _loadCompleters.clear();
     _showCompleters.clear();
+    _rewardEarnedMap.clear();
   }
 
   /// Returns the interstitial ad instance for the given app ID.
@@ -88,11 +164,24 @@ class SdkAdfurikunGateway implements AdfurikunGateway {
     return interstitial;
   }
 
+  /// Returns the reward ad instance for the given app ID.
+  ///
+  /// Throws if the app ID has not been initialized.
+  AdfurikunReward _getReward(String appId) {
+    final reward = _rewardAds[appId];
+
+    if (reward == null) {
+      throw Exception('Reward ad not initialized: $appId');
+    }
+
+    return reward;
+  }
+
   /// Creates a listener for interstitial ad callbacks.
   AdfurikunInterstitialListener _createInterstitialListener(String appId) =>
       (
         callback,
-        appId,
+        _,
         adNetworkKey,
         errorCode,
         errorMessage,
@@ -117,6 +206,45 @@ class SdkAdfurikunGateway implements AdfurikunGateway {
           case .onFailedPlaying:
             _showCompleters[appId]?.completeError(
               Exception('Failed to play interstitial ad: $appId'),
+            );
+
+          case null:
+            break;
+        }
+      };
+
+  /// Creates a listener for reward ad callbacks.
+  AdfurikunRewardListener _createRewardListener(String appId) =>
+      (
+        callback,
+        _,
+        adNetworkKey,
+        errorCode,
+        errorMessage,
+        adNetworkErrorList,
+        isRewarded,
+      ) {
+        switch (callback) {
+          case .onPrepareSuccess:
+            _loadCompleters[appId]?.complete(unit);
+
+          case .onPrepareFailure:
+            _loadCompleters[appId]?.completeError(
+              Exception('Failed to load reward ad: $appId'),
+            );
+
+          case .onStartPlaying:
+            break;
+
+          case .onFinishedPlaying:
+            _rewardEarnedMap[appId] = isRewarded ?? false;
+
+          case .onAdClose:
+            _showCompleters[appId]?.complete(unit);
+
+          case .onFailedPlaying:
+            _showCompleters[appId]?.completeError(
+              Exception('Failed to play reward ad: $appId'),
             );
 
           case null:
--- lib/domain/adfurikun/adfurikun_gateway.dart
+++ lib/domain/adfurikun/adfurikun_gateway.dart
@@ -6,7 +6,7 @@ import 'package:result_dart/result_dart.dart';
 abstract class AdfurikunGateway {
   /// Initializes an interstitial ad instance for the given app ID.
   ///
-  /// Creates an [AdfurikunInterstitial] instance and sets up its listener.
+  /// Creates an ad instance and sets up its listener.
   AsyncResult<Unit> initializeInterstitialAd({required String appId});
 
   /// Loads an interstitial ad for the given app ID.
@@ -18,6 +18,25 @@ abstract class AdfurikunGateway {
   ///
   /// Completes with [Unit] when the ad is closed.
   AsyncResult<Unit> showInterstitialAd({required String appId});
+
+  /// Initializes a reward ad instance for the given app ID.
+  ///
+  /// Creates an ad instance and sets up its listener.
+  AsyncResult<Unit> initializeRewardAd({required String appId});
+
+  /// Loads a reward ad for the given app ID.
+  ///
+  /// Completes with [Unit] when the ad is loaded and ready to be shown.
+  AsyncResult<Unit> loadRewardAd({required String appId});
+
+  /// Shows a reward ad for the given app ID.
+  ///
+  /// Completes with [Unit] when the ad is closed.
+  AsyncResult<Unit> showRewardAd({required String appId});
+
+  /// Whether the user earned a reward from the last shown ad
+  /// for the given app ID.
+  bool isRewardEarned({required String appId});
 
   /// Cleans up all ad instances.
   void dispose();
