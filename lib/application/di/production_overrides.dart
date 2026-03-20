38010068c592cf584f3ad482dfa4f19136f8d94b
Masahiko Kobayashi <kobaryubi6@gmail.com>
2026-03-20 14:14:31 +0900

    ✨ add production flavor overrides for DI

---
+++ lib/application/di/production_overrides.dart
@@ -0,0 +1,15 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_lab/application/di/provider.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+/// Provider overrides for the production flavor.
+List<Override> productionOverrides() => [
+  dioProvider.overrideWithValue(
+    Dio(
+      BaseOptions(
+        contentType: Headers.jsonContentType,
+        baseUrl: 'https://api.example.com',
+      ),
+    ),
+  ),
+];
