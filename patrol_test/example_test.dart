7553428add2f0cb2133bbcd67d2c9e95ce086395
Masahiko Kobayashi <kobaryubi6@gmail.com>
2026-03-21 16:07:39 +0900

    ✨ add Patrol example test for setup verification

--- patrol_test/example_test.dart
+++ patrol_test/example_test.dart
@@ -1,1 +1,26 @@
+import 'package:flutter/widgets.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:patrol/patrol.dart';
 
+void main() {
+  patrolTest(
+    'verify Patrol setup by going to home screen',
+    ($) async {
+      await $.pumpWidgetAndSettle(
+        WidgetsApp(
+          color: const Color(0xffffffff),
+          builder: (context, child) => const Center(
+            child: Text(
+              'app',
+              textDirection: TextDirection.ltr,
+            ),
+          ),
+        ),
+      );
+
+      expect($('app'), findsOneWidget);
+
+      await $.platform.mobile.pressHome();
+    },
+  );
+}
