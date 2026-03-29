203247b061f7c52d4a0c9d5b8f29aa0583d961d2
Masahiko Kobayashi <kobaryubi6@gmail.com>
2026-03-29 17:09:13 +0900

    ✨ add AdUnitId constants for AppLovin MAX ad unit IDs

---
+++ lib/domain/max/ad_unit_id.dart
@@ -0,0 +1,8 @@
+/// Constants for AppLovin MAX ad unit IDs per platform.
+abstract final class AdUnitId {
+  /// Rewarded ad unit ID for iOS.
+  static const rewardedIos = 'TODO-IOS-REWARDED-AD-UNIT-ID';
+
+  /// Rewarded ad unit ID for Android.
+  static const rewardedAndroid = 'TODO-ANDROID-REWARDED-AD-UNIT-ID';
+}
