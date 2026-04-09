0447ac21e09981c9379dd6192f9fda4c641b651f
Masahiko Kobayashi <kobaryubi6@gmail.com>
2026-04-09 09:37:05 +0900

    🔧 add visibility_detector package dependency

--- lib/domain/use_cases/push_notification/on_push_token_refresh_use_case.dart
+++ lib/domain/use_cases/push_notification/on_push_token_refresh_use_case.dart
@@ -1,14 +1,14 @@
 import 'package:flutter_lab/domain/push_notification/push_notification_repository.dart';
 
-/// Provides a stream of FCM token refresh events.
-class OnTokenRefreshUseCase {
-  OnTokenRefreshUseCase({
+/// Provides a stream of push token refresh events.
+class OnPushTokenRefreshUseCase {
+  OnPushTokenRefreshUseCase({
     required PushNotificationRepository pushNotificationRepository,
   }) : _pushNotificationRepository = pushNotificationRepository;
 
   final PushNotificationRepository _pushNotificationRepository;
 
-  /// Returns a stream that emits a new token whenever the FCM token
+  /// Returns a stream that emits a new token whenever the push token
   /// is refreshed.
-  Stream<String> call() => _pushNotificationRepository.onTokenRefresh;
+  Stream<String> call() => _pushNotificationRepository.onPushTokenRefresh;
 }
