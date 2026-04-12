fdf56fa5258bca425f2ea03d926619f87e152db2
Masahiko Kobayashi <kobaryubi6@gmail.com>
2026-04-12 14:25:21 +0900

    🔥 remove unused button_style_button, elevated_button, and text_button

--- lib/ui/core/ui/elevated_button.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_lab/ui/core/ui/button_style_button.dart';
-
-class ElevatedButton extends ButtonStyleButton {
-  const ElevatedButton({
-    required super.onPressed,
-    required super.child,
-    super.key,
-  });
-}
--- lib/ui/core/ui/text_button.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter_lab/ui/core/ui/button_style_button.dart';
-
-class TextButton extends ButtonStyleButton {
-  const TextButton({required super.onPressed, required super.child, super.key});
-}
