ddb07b67e073518c49f17083159e9a805285093f
Masahiko Kobayashi <kobaryubi6@gmail.com>
2026-04-20 15:39:18 +0900

    Revert "✨ implement processAndResolveDeeplink in MockAdjustGateway"
    This reverts commit 3142897fe2766b10ec80c35c566be6f02c7d4fa7.

--- lib/data/gateway/adjust/mock_adjust_gateway.dart
+++ lib/data/gateway/adjust/mock_adjust_gateway.dart
@@ -15,9 +15,5 @@ class MockAdjustGateway implements AdjustGateway {
       const Success(unit);
 
   @override
-  AsyncResult<String> processAndResolveDeeplink({required String url}) async =>
-      Success(url);
-
-  @override
   void setPushToken({required String token}) {}
 }
