c23aa8ba9ba78e1dfb3bdea3020b0c873c6e00e8
Masahiko Kobayashi <kobaryubi6@gmail.com>
2026-05-16 20:46:52 +0900

    ✨ add initialUserScripts to the WebView wrapper

--- lib/ui/http_only_cookie/widget/web_view.dart
+++ lib/ui/http_only_cookie/widget/web_view.dart
@@ -1,3 +1,5 @@
+import 'dart:collection';
+
 import 'package:flutter/widgets.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:flutter_lab/ui/http_only_cookie/hook/use_in_app_web_view.dart';
@@ -7,7 +9,12 @@ import 'package:flutter_lab/ui/http_only_cookie/hook/use_in_app_web_view.dart';
 /// Wires the WebView to the [state] produced by `useInAppWebView`,
 /// capturing the controller through `onWebViewCreated`.
 class WebView extends StatelessWidget {
-  const WebView({required this.state, required this.url, super.key});
+  const WebView({
+    required this.state,
+    required this.url,
+    this.initialUserScripts,
+    super.key,
+  });
 
   /// State produced by the `useInAppWebView` hook.
   final InAppWebViewState state;
@@ -15,9 +22,13 @@ class WebView extends StatelessWidget {
   /// URL loaded by the WebView.
   final String url;
 
+  /// User scripts injected when the WebView is created.
+  final UnmodifiableListView<UserScript>? initialUserScripts;
+
   @override
   Widget build(BuildContext context) => InAppWebView(
     initialUrlRequest: URLRequest(url: WebUri(url)),
+    initialUserScripts: initialUserScripts,
     initialSettings: InAppWebViewSettings(),
     onWebViewCreated: state.onWebViewCreated,
     onLoadStop: state.onLoadStop,
