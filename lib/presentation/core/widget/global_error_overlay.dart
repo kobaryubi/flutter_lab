51c467506e4cedc24db611ac07fae8ddeead8250
Masahiko Kobayashi <kobaryubi6@gmail.com>
2026-07-18 13:38:06 +0900

    ✨ add BackButtonSwallowObserver to GlobalErrorOverlay for improved navigation handling

--- lib/presentation/core/widget/global_error_overlay.dart
+++ lib/presentation/core/widget/global_error_overlay.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/widgets.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:flutter_lab/presentation/core/observer/back_button_swallow_observer.dart';
 import 'package:flutter_lab/presentation/core/provider/global_error_widget_notifier.dart';
 import 'package:flutter_portal/flutter_portal.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
@@ -13,6 +15,14 @@ class GlobalErrorOverlay extends HookConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final widget = ref.watch(globalErrorWidgetProvider);
     final isVisible = widget != null;
+
+    final observer = useMemoized(BackButtonSwallowObserver.new);
+
+    useEffect(() {
+      WidgetsBinding.instance.addObserver(observer);
+
+      return () => WidgetsBinding.instance.removeObserver(observer);
+    }, [observer]);
 
     return PortalTarget(
       visible: isVisible,
